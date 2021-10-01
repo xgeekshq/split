4c8bbf7e21e3e587552935b665fe3cda40ad89d7
Nuno Caseiro <n.caseiro@kigroup.de>
2021-10-01 14:31:50 +0100

    refactor: moved main styled component to outside of layout's react function

--- frontend/components/Layout/Layout.tsx
+++ frontend/components/Layout/Layout.tsx
@@ -1,10 +1,8 @@
 import { styled } from "../../stitches.config";
 
+const Main = styled("main", { display: "block", px: "$40" });
+
 const Layout: React.FC = ({ children }) => {
-  const Main = styled("main", { display: "block", px: "$40" });
-
-  // Navbar, Main and footer and footer
-
   return (
     <>
       <Main>{children}</Main>
