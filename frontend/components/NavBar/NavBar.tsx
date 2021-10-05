bdad7fc9a430152ae5343a1be42c178bfbffc881
Nuno Caseiro <n.caseiro@kigroup.de>
2021-10-05 16:42:19 +0100

    refactor: added component to handle boards title in different pages

--- frontend/components/NavBar/NavBar.tsx
+++ frontend/components/NavBar/NavBar.tsx
@@ -1,22 +1,18 @@
 import Link from "next/link";
-import { useContext } from "react";
 import { styled } from "../../stitches.config";
 import Flex from "../Primitives/Flex";
 import Text from "../Primitives/Text";
-import TitleContext from "../../store/title-context";
 import Dropdown from "./DropdownMenu";
+import Title from "./Title";
 
 const Header = styled("header", Flex, {
   px: "$40",
-  mb: "$40",
   py: "$8",
   backgroundColor: "White",
   boxShadow: "0 2px 4px 0 rgba(0,0,0,.2)",
 });
 
 const NavBar: React.FC = () => {
-  const titleCtx = useContext(TitleContext);
-
   return (
     <Header justify="between" align="center" media="sm">
       <Link href="/">
@@ -24,9 +20,7 @@ const NavBar: React.FC = () => {
           Divide & conquer
         </Text>
       </Link>
-      <Text fontWeight="semiBold" size="xl">
-        {titleCtx.title}
-      </Text>
+      <Title />
       <Dropdown />
     </Header>
   );
