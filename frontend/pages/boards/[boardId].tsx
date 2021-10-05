c3c6a52b06ac99293dd218031170431d2b5903d1
Nuno Caseiro <n.caseiro@kigroup.de>
2021-10-05 22:40:42 +0100

    feat: start development of the save button. edit boards title

--- frontend/pages/boards/[boardId].tsx
+++ frontend/pages/boards/[boardId].tsx
@@ -1,13 +1,42 @@
-import { useContext, useEffect } from "react";
+import { GetStaticProps, GetStaticPaths } from "next";
+import { useContext, useEffect, useRef } from "react";
 import TitleContext from "../../store/title-context";
 
-const Board: React.FC = () => {
-  const context = useContext(TitleContext);
+export const getStaticProps: GetStaticProps = async (context) => {
+  return {
+    props: {
+      slug: context.params?.boardId,
+    },
+  };
+};
 
-  useEffect(() => console.log("new"), [context.title]);
+// hardcoded while we dont have backend
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { boardId: "new-board" } }],
+    fallback: false,
+  };
+};
+
+const Board: React.FC<{ slug: string }> = ({ slug }) => {
+  const titleCtx = useContext(TitleContext);
+
+  const isFirstRun = useRef(true);
+
+  useEffect(() => {
+    if (isFirstRun.current) {
+      titleCtx.setTitle(slug);
+      isFirstRun.current = false;
+      console.log("1");
+    } else if (!isFirstRun.current) {
+      // update on server
+      console.log("2");
+    }
+  }, [slug, titleCtx]);
+
   return (
     <>
-      <button type="button" onClick={() => context.setTitle(Math.random().toString())}>
+      <button type="button" onClick={() => titleCtx.setTitle(Math.random().toString())}>
         aaaa
       </button>
     </>
