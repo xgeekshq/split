bccf862c04cc594642477f056af2c8b9cf3e3912
Nuno Caseiro <n.caseiro@kigroup.de>
2021-10-05 16:19:38 +0100

    refactor: removed handle title of context

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -1,4 +1,13 @@
+import { useEffect, useContext } from "react";
+import TitleContext from "../store/title-context";
+
 const Dashboard: React.FC = () => {
+  const titleCtx = useContext(TitleContext);
+
+  useEffect(() => {
+    titleCtx.setTitle("Dashboard");
+  }, [titleCtx]);
+
   return <>Dashboard</>;
 };
 
