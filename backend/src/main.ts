d530056ca530e48b1b2806bc1fbdfd77ff8299b2
Nuno Caseiro <n.caseiro@kigroup.de>
2021-10-27 15:00:55 +0100

    refactor: added port constant

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PORT } from './constants/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
+  await app.listen(PORT);
 }
 bootstrap();
