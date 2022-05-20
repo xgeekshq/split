87de26916527df393caaed086304f347fde4f50a
Daniel Sousa <d.sousa@kigroup.de>
2022-05-20 14:29:58 +0100

    fix: SWC tests error (second attempt)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,25 +1,25 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
 
-require("dotenv").config({ path: "../.env" });
-const webpack = require("webpack");
+require('dotenv').config({ path: '../.env' });
+const webpack = require('webpack');
 
 module.exports = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    outputStandalone: true,
-  },
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(process.env));
+	reactStrictMode: true,
+	swcMinify: false,
+	experimental: {
+		outputStandalone: true
+	},
+	webpackDevMiddleware: (config) => {
+		config.watchOptions = {
+			poll: 1000,
+			aggregateTimeout: 300
+		};
+		return config;
+	},
+	webpack(config) {
+		config.plugins.push(new webpack.EnvironmentPlugin(process.env));
 
-    return config;
-  },
+		return config;
+	}
 };
