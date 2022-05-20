7100fc10a69d7ae4a453087985bf3fec45557dba
Daniel Sousa <d.sousa@kigroup.de>
2022-05-20 16:38:58 +0100

    fix: next auth types

--- frontend/types/next-auth.d.ts
+++ frontend/types/next-auth.d.ts
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { DefaultSession } from 'next-auth';
 
 declare module 'next-auth' {
 	interface Session {
 		user: {
+			id: string;
 			firstName: string;
 			lastName: string;
 			email: string;
-			id: string;
-		};
+		} & DefaultSession['user'];
 		accessToken: string;
 		refreshToken?: string;
 		expires: number;
