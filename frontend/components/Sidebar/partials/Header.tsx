eee9c5c5611fc8a0f99ad5282579f9394efea789
Daniel Sousa <d.sousa@kigroup.de>
2022-05-20 15:02:35 +0100

    fix: imports paths

--- frontend/components/Sidebar/partials/Header.tsx
+++ frontend/components/Sidebar/partials/Header.tsx
@@ -1,70 +1,70 @@
-import { FC } from "react";
-import { SidebarHeader } from "react-pro-sidebar";
-import Flex from "../../Primitives/Flex";
-import HeaderBannerIcon from "../../icons/HeaderBanner";
-import Text from "../../Primitives/Text";
-import Separator from "./Separator";
-import { styled } from "../../stitches.config";
-import Icon from "../icons/Icon";
+import { FC } from 'react';
+import { SidebarHeader } from 'react-pro-sidebar';
+import Flex from '../../Primitives/Flex';
+import HeaderBannerIcon from '../../icons/HeaderBanner';
+import Text from '../../Primitives/Text';
+import Separator from './Separator';
+import Icon from '../../icons/Icon';
+import { styled } from '../../../stitches.config';
 
 const StyledHeader = styled(SidebarHeader, Flex);
 
 const Header: FC<{ firstName: string; lastName: string; email: string }> = ({
-  firstName,
-  lastName,
-  email,
+	firstName,
+	lastName,
+	email
 }) => {
-  const initialLetters = firstName.charAt(0) + lastName.charAt(0);
+	const initialLetters = firstName.charAt(0) + lastName.charAt(0);
 
-  return (
-    <StyledHeader direction="column">
-      <Flex css={{ p: "$40" }}>
-        <HeaderBannerIcon />
-      </Flex>
-      <Separator />
-      <Flex
-        align="center"
-        gap="12"
-        css={{
-          p: "$24",
-          color: "$white",
-          backgroundColor: "$primary700",
-        }}
-      >
-        <Flex
-          css={{ width: "57px", height: "58px", position: "relative" }}
-          align="center"
-          justify="center"
-        >
-          <Icon
-            name="userIcon"
-            css={{
-              width: "100%",
-              height: "100%",
-              position: "absolute",
-              top: "0",
-              bottom: "0",
-              zIndex: "0",
-              left: "0",
-              right: "0",
-            }}
-          />
-          <Text size="md" color="primary800" weight="bold" css={{ zIndex: 1 }}>
-            {initialLetters}
-          </Text>
-        </Flex>
-        <Flex direction="column">
-          <Text css={{ color: "$white" }} weight="medium" size="sm">
-            {`${firstName} ${lastName}`}
-          </Text>
-          <Text css={{ color: "$primary200" }} weight="medium" size="xs">
-            {email}
-          </Text>
-        </Flex>
-      </Flex>
-      <Separator />
-    </StyledHeader>
-  );
+	return (
+		<StyledHeader direction="column">
+			<Flex css={{ p: '$40' }}>
+				<HeaderBannerIcon />
+			</Flex>
+			<Separator />
+			<Flex
+				align="center"
+				gap="12"
+				css={{
+					p: '$24',
+					color: '$white',
+					backgroundColor: '$primary700'
+				}}
+			>
+				<Flex
+					css={{ width: '57px', height: '58px', position: 'relative' }}
+					align="center"
+					justify="center"
+				>
+					<Icon
+						name="userIcon"
+						css={{
+							width: '100%',
+							height: '100%',
+							position: 'absolute',
+							top: '0',
+							bottom: '0',
+							zIndex: '0',
+							left: '0',
+							right: '0'
+						}}
+					/>
+					<Text size="md" color="primary800" weight="bold" css={{ zIndex: 1 }}>
+						{initialLetters}
+					</Text>
+				</Flex>
+				<Flex direction="column">
+					<Text css={{ color: '$white' }} weight="medium" size="sm">
+						{`${firstName} ${lastName}`}
+					</Text>
+					<Text css={{ color: '$primary200' }} weight="medium" size="xs">
+						{email}
+					</Text>
+				</Flex>
+			</Flex>
+			<Separator />
+		</StyledHeader>
+	);
 };
 
 export default Header;
