99279f5414dc736f04a533d1cabcb9fb75c9f3bb
Catia Barroco <c.barroco@kigroup.de>
2022-05-25 17:18:15 +0100

    fix: eslint errors

--- frontend/pages/500.tsx
+++ frontend/pages/500.tsx
@@ -1,8 +1,14 @@
-import Link from "next/link";
-import Banner from "../components/icons/Banner";
-import LogoIcon from "../components/icons/Logo";
-import Text from "../components/Primitives/Text";
-import { BannerContainer, ContainerSection, GoBackButton, ImageBackground } from "../styles/pages/error.styles";
+import Link from 'next/link';
+
+import Banner from '../components/icons/Banner';
+import LogoIcon from '../components/icons/Logo';
+import Text from '../components/Primitives/Text';
+import {
+	BannerContainer,
+	ContainerSection,
+	GoBackButton,
+	ImageBackground
+} from '../styles/pages/error.styles';
 
 const Custom500 = () => {
 	return (
@@ -14,19 +20,19 @@ const Custom500 = () => {
 			<ContainerSection>
 				<LogoIcon />
 
-				<Text size={'xl'} css={{ mt: '$29', fontSize: '$48' }} heading="1">
+				<Text size="xl" css={{ mt: '$29', fontSize: '$48' }} heading="1">
 					500
 				</Text>
 
-				<Text css={{ mt: '$10' }} heading="2" weight={'medium'}>
+				<Text css={{ mt: '$10' }} heading="2" weight="medium">
 					Server Error
 				</Text>
-				<Text size="md" color={'primary500'} css={{ mt: '$24' }}>
+				<Text size="md" color="primary500" css={{ mt: '$24' }}>
 					Try to refresh this page or feel free to contact us if the problem persists.
 				</Text>
 				<Link href="/">
 					<GoBackButton
-						variant={'primary'}
+						variant="primary"
 						size="md"
 						css={{ mt: '$26' }}
 						style={{ width: '100%' }}
