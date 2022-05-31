5566f453b6f01367bae9799006834944618a887d
Daniel Sousa <d.sousa@kigroup.de>
2022-05-31 17:24:54 +0100

    fix: eslint config and require

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -26,7 +26,7 @@ module.exports = {
 		ecmaVersion: 12,
 		sourceType: 'module',
 		extraFileExtensions: ['.css'],
-		project: ['./tsconfig.json', './cypress/tsconfig.json']
+		project: './tsconfig.json'
 	},
 	plugins: [
 		'react',
