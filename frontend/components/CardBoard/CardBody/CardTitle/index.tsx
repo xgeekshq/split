aa41a7db171d71b14d521ac7dd7e41cfa22de4fc
Daniel Sousa <d.sousa@kigroup.de>
2022-06-01 15:45:14 +0100

    fix: create title with conditions in a separated component

---
+++ frontend/components/CardBoard/CardBody/CardTitle/index.tsx
@@ -0,0 +1,45 @@
+import { Title } from '@radix-ui/react-dialog';
+
+import Tooltip from '../../../Primitives/Tooltip';
+
+type CardTitleProps = {
+	userIsParticipating: boolean;
+	boardId: string;
+	title: string;
+	isSubBoard: boolean | undefined;
+	mainBoardId?: string;
+};
+
+const CardTitle: React.FC<CardTitleProps> = ({
+	userIsParticipating,
+	boardId,
+	title,
+	isSubBoard,
+	mainBoardId
+}) => {
+	return isSubBoard ? (
+		<Tooltip content="It’s a sub-team board. A huge team got splitted into sub teams.">
+			<Title
+				userIsParticipating={userIsParticipating}
+				boardId={boardId}
+				title={title}
+				isSubBoard={isSubBoard}
+				mainBoardId={mainBoardId}
+			/>
+		</Tooltip>
+	) : (
+		<Title
+			userIsParticipating={userIsParticipating}
+			boardId={boardId}
+			title={title}
+			isSubBoard={isSubBoard}
+			mainBoardId={mainBoardId}
+		/>
+	);
+};
+
+CardTitle.defaultProps = {
+	mainBoardId: undefined
+};
+
+export default CardTitle;
---
+++ frontend/components/CardBoard/CardBody/CardTitle/partials/Title/index.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+
+import { StyledBoardTitle } from './styles';
+
+type Props = {
+	userIsParticipating: boolean;
+	boardId: string;
+	title: string;
+	isSubBoard: boolean | undefined;
+	mainBoardId?: string;
+};
+
+const Title = ({ userIsParticipating, boardId, title, isSubBoard, mainBoardId }: Props) => {
+	return userIsParticipating ? (
+		<Link
+			key={boardId}
+			href={{
+				pathname: `boards/[boardId]`,
+				query: isSubBoard ? { boardId, mainBoardId } : { boardId }
+			}}
+		>
+			<StyledBoardTitle data-disabled={!userIsParticipating}>{title}</StyledBoardTitle>
+		</Link>
+	) : (
+		<StyledBoardTitle data-disabled={!userIsParticipating}>{title}</StyledBoardTitle>
+	);
+};
+
+Title.defaultProps = {
+	mainBoardId: undefined
+};
+
+export { Title };
