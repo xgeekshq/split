e73c2b910882747882f0531d516461b53cf4a3dc
Rafael Matos <100219341+r-dmatos@users.noreply.github.com>
2022-06-13 11:12:30 +0100

    [FEATURE]: Remaining Votes Toast on Board (#227)
    * feat: remaining votes toasts
    
    * fix: update browser title to split
    
    * feat: remaining votes toasts
    
    * fix: update browser title to split
    
    * chore: pr #227 suggestions
    
    * chore: conflit fix
    
    Co-authored-by: Daniel Sousa <d.sousa@kigroup.de>
    Co-authored-by: Daniel Sousa <104842894+dsousa12@users.noreply.github.com>

--- frontend/src/hooks/useVotes.tsx
+++ frontend/src/hooks/useVotes.tsx
@@ -1,4 +1,5 @@
 import { useMutation } from 'react-query';
+import { useSession } from 'next-auth/react';
 
 import { addVoteRequest, deleteVoteRequest } from '../api/boardService';
 import { ToastStateEnum } from '../utils/enums/toast-types';
@@ -6,10 +7,19 @@ import useBoardUtils from './useBoardUtils';
 
 const useVotes = () => {
 	const { queryClient, setToastState } = useBoardUtils();
+	const { data: session } = useSession({ required: true });
+	const userId = session?.user?.id;
 
 	const addVote = useMutation(addVoteRequest, {
 		onSuccess: (data) => {
 			queryClient.invalidateQueries(['board', { id: data?._id }]);
+			const votesByUser = data.users.find((user) => user.user._id === userId)?.votesCount;
+			const remainingVotes = data.maxVotes - (votesByUser ?? 0);
+			setToastState({
+				open: true,
+				content: `You have ${remainingVotes} votes left`,
+				type: ToastStateEnum.INFO
+			});
 		},
 		onError: () => {
 			setToastState({
@@ -23,6 +33,13 @@ const useVotes = () => {
 	const deleteVote = useMutation(deleteVoteRequest, {
 		onSuccess: (data) => {
 			queryClient.invalidateQueries(['board', { id: data?._id }]);
+			const votesByUser = data.users.find((user) => user.user._id === userId)?.votesCount;
+			const remainingVotes = data.maxVotes - (votesByUser ?? 0);
+			setToastState({
+				open: true,
+				content: `Vote removed. You have ${remainingVotes} votes left.`,
+				type: ToastStateEnum.INFO
+			});
 		},
 		onError: () => {
 			setToastState({
--- frontend/src/types/board/board.user.ts
+++ frontend/src/types/board/board.user.ts
@@ -2,6 +2,7 @@ import { BoardUserRoles } from 'utils/enums/board.user.roles';
 import { User } from '../user/user';
 
 export interface BoardUser {
+	id: any;
 	user: User;
 	role: BoardUserRoles;
 	_id: string;
