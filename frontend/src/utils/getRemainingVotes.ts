b0b976bd792315f5b717904f5a755b4d1b09ee15
Daniel Sousa <d.sousa@kigroup.de>
2022-06-27 06:59:58 +0100

    fix: use number instead parse int

--- frontend/src/utils/getRemainingVotes.ts
+++ frontend/src/utils/getRemainingVotes.ts
@@ -3,7 +3,7 @@ import BoardType from '../types/board/board';
 const getRemainingVotes = (data: BoardType, userId: string) => {
 	const votesByUser = data.users.find((user) => user.user._id === userId)?.votesCount;
 
-	return (data.maxVotes ? parseInt(data.maxVotes, 10) : 0) - (votesByUser ?? 0);
+	return Number(data.maxVotes) - (votesByUser ?? 0);
 };
 
 export { getRemainingVotes };
