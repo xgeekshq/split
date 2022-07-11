f0c8a90b061aea22288b4bf1fc9f961c36da7b2e
Geomar Bastiani <g.bastiani@kigroup.de>
2022-07-11 13:12:01 +0100

    feat: add team.filter.options lib

---
+++ backend/src/libs/dto/param/team.filter.options.ts
@@ -0,0 +1,16 @@
+import { IsBoolean, IsEnum, IsOptional, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+import { TeamRoles } from 'src/libs/enum/team.roles';
+
+export class TeamFilterOptions {
+  @IsString()
+  @IsOptional()
+  @IsEnum(TeamRoles, { each: true })
+  teamUserRole?: TeamRoles;
+
+  @IsOptional()
+  @IsBoolean()
+  @Transform(({ value }) => [true, 'true', '', '1'].includes(value))
+  loadUsers?: boolean;
+}
