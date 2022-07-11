2ba5d32438a393227026f5ea8317654c3d23d75c
Geomar Bastiani <g.bastiani@kigroup.de>
2022-07-11 13:30:16 +0100

    feat: add filters loadUsers and userTeamRole to getTeam and remove GET /teams/member

--- backend/src/modules/teams/controller/team.controller.ts
+++ backend/src/modules/teams/controller/team.controller.ts
@@ -7,10 +7,14 @@ import {
   Param,
   Post,
   Put,
+  Query,
   Req,
   UseGuards,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { TeamParams } from 'src/libs/dto/param/team.params';
+import { TeamFilterOptions } from 'src/libs/dto/param/team.filter.options';
 import { INSERT_FAILED } from '../../../libs/exceptions/messages';
 import JwtAuthenticationGuard from '../../../libs/guards/jwtAuth.guard';
 import RequestWithUser from '../../../libs/interfaces/requestWithUser.interface';
@@ -53,19 +57,11 @@ export default class TeamsController {
 
   @UseGuards(JwtAuthenticationGuard)
   @Get(':teamId')
-  getTeam(@Param() { teamId }: TeamParams) {
-    return this.getTeamApp.getTeam(teamId);
-  }
-
-  @UseGuards(JwtAuthenticationGuard)
-  @Get(':teamId/stakeholders')
-  getTeamStakeholders(@Param() { teamId }: TeamParams) {
-    return this.getTeamApp.getTeamStakeholders(teamId);
-  }
-
-  @UseGuards(JwtAuthenticationGuard)
-  @Get('/member')
-  getTeamsOfUser(@Req() request: RequestWithUser) {
-    return this.getTeamApp.getTeamsOfUser(request.user._id);
+  @UsePipes(new ValidationPipe({ transform: true }))
+  getTeam(
+    @Param() { teamId }: TeamParams,
+    @Query() teamFilterOptions?: TeamFilterOptions,
+  ) {
+    return this.getTeamApp.getTeam(teamId, teamFilterOptions);
   }
 }
