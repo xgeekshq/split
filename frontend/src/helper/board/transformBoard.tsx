2cc71b82a6420f7e664fbfdf8e418b8cca479dc7
João Santos <68588265+joaosantos99@users.noreply.github.com>
2022-07-18 09:56:07 +0100

    [FEATURE]: Optimistic Updates - Comments (#334)
    * feat: optimistic comments
    
    * fix: pr conflit

--- frontend/src/helper/board/transformBoard.tsx
+++ frontend/src/helper/board/transformBoard.tsx
@@ -5,6 +5,9 @@ import DeleteCardDto from 'types/card/deleteCard.dto';
 import RemoveFromCardGroupDto from 'types/card/removeFromCardGroup.dto';
 import UpdateCardDto from 'types/card/updateCard.dto';
 import UpdateCardPositionDto from 'types/card/updateCardPosition.dto';
+import AddCommentDto from 'types/comment/addComment.dto';
+import DeleteCommentDto from 'types/comment/deleteComment.dto';
+import UpdateCommentDto from 'types/comment/updateComment.dto';
 import { addElementAtIndex, removeElementAtIndex } from 'utils/array';
 
 // avoid read only error
@@ -103,5 +106,81 @@ export const handleUnMergeCard = (board: BoardType, changes: RemoveFromCardGroup
 		});
 	}
 
+	return boardData;
+};
+
+export const handleAddComments = (board: BoardType, changes: AddCommentDto, user: any) => {
+	// avoid read only error
+	const boardData: BoardType = JSON.parse(JSON.stringify(board));
+	const { cardId, cardItemId, text } = changes;
+	let columnIndex = 0;
+
+	boardData.columns.forEach((item, index) => {
+		if (item.cards.find((col) => col._id === cardId) !== undefined) {
+			return (columnIndex = index);
+		}
+	});
+
+	const card = boardData.columns[columnIndex].cards.find((card) => card._id === cardId);
+	const cardItem = card!.items.find((cardItem) => cardItem._id === cardItemId);
+	const placehodlerId = (Math.random() + 1).toString(36).substring(7);
+
+	const commentObj = {
+		text: text,
+		createdBy: {
+			_id: user.id,
+			firstName: user.firstName,
+			lastName: user.lastName,
+			email: '',
+			isSAdmin: false,
+			joinedAt: ''
+		},
+		_id: placehodlerId,
+		id: placehodlerId
+	};
+
+	cardItem?.comments.push(commentObj);
+
+	return boardData;
+};
+
+export const handleUpdateComments = (board: BoardType, changes: UpdateCommentDto) => {
+	// avoid read only error
+	const boardData: BoardType = JSON.parse(JSON.stringify(board));
+	const { cardId, cardItemId, commentId, text } = changes;
+	let columnIndex = 0;
+
+	boardData.columns.forEach((item, index) => {
+		if (item.cards.find((col) => col._id === cardId) !== undefined) {
+			return (columnIndex = index);
+		}
+	});
+
+	const card = boardData.columns[columnIndex].cards.find((card) => card._id === cardId);
+	const cardItem = card!.items.find((cardItem) => cardItem._id === cardItemId);
+	const comment = cardItem!.comments.find((comment) => comment._id === commentId);
+
+	comment!.text = text;
+	return boardData;
+};
+
+export const handleDeleteComments = (board: BoardType, changes: DeleteCommentDto) => {
+	// avoid read only error
+	const boardData: BoardType = JSON.parse(JSON.stringify(board));
+	const { cardId, cardItemId, commentId } = changes;
+	let columnIndex = 0;
+
+	boardData.columns.forEach((item, index) => {
+		if (item.cards.find((col) => col._id === cardId) !== undefined) {
+			return (columnIndex = index);
+		}
+	});
+
+	const card = boardData.columns[columnIndex].cards.find((card) => card._id === cardId);
+	const cardItem = card!.items.find((cardItem) => cardItem._id === cardItemId);
+	const comment = cardItem!.comments.find((comment) => comment._id === commentId);
+	const commentIndex = cardItem?.comments.indexOf(comment!, 0);
+
+	cardItem?.comments.splice(commentIndex!, 1);
 	return boardData;
 };
