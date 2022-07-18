9d25e41f5aa1dd0d709aa5f25158ea1ad4f16067
joaosantos99 <jfss291@gmail.com>
2022-07-18 08:54:47 +0100

    feat: optimistic comments

--- frontend/src/helper/board/transformBoard.tsx
+++ frontend/src/helper/board/transformBoard.tsx
@@ -1,5 +1,8 @@
 import BoardType from 'types/board/board';
 import UpdateCardPositionDto from 'types/card/updateCardPosition.dto';
+import AddCommentDto from 'types/comment/addComment.dto';
+import DeleteCommentDto from 'types/comment/deleteComment.dto';
+import UpdateCommentDto from 'types/comment/updateComment.dto';
 import { addElementAtIndex, removeElementAtIndex } from 'utils/array';
 
 export const handleUpdateCardPosition = (board: BoardType, changes: UpdateCardPositionDto) => {
@@ -15,5 +18,83 @@ export const handleUpdateCardPosition = (board: BoardType, changes: UpdateCardPo
 		colToRemove.cards = removeElementAtIndex(colToRemove.cards, cardPosition);
 		colToAdd.cards = addElementAtIndex(colToAdd.cards, newPosition, cardToAdd);
 	}
+	return boardData;
+};
+
+export const handleAddComments = (board: BoardType, changes: AddCommentDto, user: any) => {
+	// avoid read only error
+	const boardData: BoardType = JSON.parse(JSON.stringify(board));
+	const { cardId, cardItemId, text } = changes;
+	let columnIndex = 0;
+
+	boardData.columns.forEach((item, index) => {
+		if (item.cards.find((col) => col._id === cardId) !== undefined) {
+			return (columnIndex = index);
+		}
+	});
+
+	const card = boardData.columns[columnIndex].cards.find((card) => card._id === cardId);
+	const cardItem = card!.items.find((cardItem) => cardItem._id === cardItemId);
+	const placehodlerId = (Math.random() + 1).toString(36).substring(7);
+
+	const commentObj = {
+		text: text,
+		createdBy: {
+			_id: user.id,
+			firstName: user.firstName,
+			lastName: user.lastName,
+			email: '',
+			isSAdmin: false,
+			joinedAt: ''
+		},
+		_id: placehodlerId,
+		id: placehodlerId
+	};
+
+	cardItem?.comments.push(commentObj);
+
+	return boardData;
+};
+
+export const handleDeleteComments = (board: BoardType, changes: DeleteCommentDto) => {
+	// avoid read only error
+	const boardData: BoardType = JSON.parse(JSON.stringify(board));
+	const { cardId, cardItemId, commentId } = changes;
+	let columnIndex = 0;
+
+	boardData.columns.forEach((item, index) => {
+		if (item.cards.find((col) => col._id === cardId) !== undefined) {
+			return (columnIndex = index);
+		}
+	});
+
+	const card = boardData.columns[columnIndex].cards.find((card) => card._id === cardId);
+	const cardItem = card!.items.find((cardItem) => cardItem._id === cardItemId);
+	const comment = cardItem!.comments.find((comment) => comment._id === commentId);
+	const commentIndex = cardItem?.comments.indexOf(comment!, 0);
+
+	cardItem?.comments.splice(commentIndex!, 1);
+
+	return boardData;
+};
+
+export const handleUpdateComments = (board: BoardType, changes: UpdateCommentDto) => {
+	// avoid read only error
+	const boardData: BoardType = JSON.parse(JSON.stringify(board));
+	const { cardId, cardItemId, commentId, text } = changes;
+	let columnIndex = 0;
+
+	boardData.columns.forEach((item, index) => {
+		if (item.cards.find((col) => col._id === cardId) !== undefined) {
+			return (columnIndex = index);
+		}
+	});
+
+	const card = boardData.columns[columnIndex].cards.find((card) => card._id === cardId);
+	const cardItem = card!.items.find((cardItem) => cardItem._id === cardItemId);
+	const comment = cardItem!.comments.find((comment) => comment._id === commentId);
+
+	comment!.text = text;
+
 	return boardData;
 };
--- frontend/src/hooks/useComments.tsx
+++ frontend/src/hooks/useComments.tsx
@@ -1,43 +1,151 @@
 import { useMutation } from 'react-query';
+import { useSession } from 'next-auth/react';
 
 import { addCommentRequest, deleteCommentRequest, updateCommentRequest } from '../api/boardService';
 import { ToastStateEnum } from '../utils/enums/toast-types';
 import useBoardUtils from './useBoardUtils';
+import BoardType from 'types/board/board';
+import {
+	handleUpdateComments,
+	handleDeleteComments,
+	handleAddComments
+} from 'helper/board/transformBoard';
 
 const useComments = () => {
 	const { queryClient, setToastState } = useBoardUtils();
+	const { data: session } = useSession({ required: false });
 
 	const addCommentInCard = useMutation(addCommentRequest, {
-		onSuccess: (data) => {
+		onMutate: async (data) => {
+			const user = session?.user;
+			const query = ['board', { id: data.boardId }];
+			await queryClient.cancelQueries(query);
+
+			const prevData = queryClient.getQueryData<{ board: BoardType }>(query);
+			const board = prevData?.board;
+
+			if (board) {
+				const newBoard = handleAddComments(board, data, user);
+
+				queryClient.setQueryData<{ board: BoardType } | undefined>(
+					query,
+					(old: { board: BoardType } | undefined) => {
+						if (old)
+							return {
+								board: {
+									...old.board,
+									columns: newBoard.columns
+								}
+							};
+
+						return old;
+					}
+				);
+			}
+
+			return { previousBoard: board, data };
+		},
+		onSettled: (data) => {
 			queryClient.invalidateQueries(['board', { id: data?._id }]);
 		},
-		onError: () => {
+		onSuccess: () => {},
+		onError: (data, variables, context) => {
+			queryClient.setQueryData(
+				['board', { id: variables.boardId }],
+				(context as { previousBoard: BoardType }).previousBoard
+			);
 			setToastState({
 				open: true,
-				content: 'Error adding the comment',
+				content: 'Error updating the comment',
 				type: ToastStateEnum.ERROR
 			});
 		}
 	});
 
 	const deleteComment = useMutation(deleteCommentRequest, {
-		onSuccess: (data) => {
+		onMutate: async (data) => {
+			const query = ['board', { id: data.boardId }];
+			await queryClient.cancelQueries(query);
+
+			const prevData = queryClient.getQueryData<{ board: BoardType }>(query);
+			const board = prevData?.board;
+
+			if (board) {
+				const newBoard = handleDeleteComments(board, data);
+
+				queryClient.setQueryData<{ board: BoardType } | undefined>(
+					query,
+					(old: { board: BoardType } | undefined) => {
+						if (old)
+							return {
+								board: {
+									...old.board,
+									columns: newBoard.columns
+								}
+							};
+
+						return old;
+					}
+				);
+			}
+
+			return { previousBoard: board, data };
+		},
+		onSettled: (data) => {
 			queryClient.invalidateQueries(['board', { id: data?._id }]);
 		},
-		onError: () => {
+		onSuccess: () => {},
+		onError: (data, variables, context) => {
+			queryClient.setQueryData(
+				['board', { id: variables.boardId }],
+				(context as { previousBoard: BoardType }).previousBoard
+			);
 			setToastState({
 				open: true,
-				content: 'Error deleting the comment',
+				content: 'Error updating the comment',
 				type: ToastStateEnum.ERROR
 			});
 		}
 	});
 
 	const updateComment = useMutation(updateCommentRequest, {
-		onSuccess: (data) => {
+		onMutate: async (data) => {
+			const query = ['board', { id: data.boardId }];
+			await queryClient.cancelQueries(query);
+
+			const prevData = queryClient.getQueryData<{ board: BoardType }>(query);
+			const board = prevData?.board;
+
+			if (board) {
+				const newBoard = handleUpdateComments(board, data);
+
+				queryClient.setQueryData<{ board: BoardType } | undefined>(
+					query,
+					(old: { board: BoardType } | undefined) => {
+						if (old)
+							return {
+								board: {
+									...old.board,
+									columns: newBoard.columns
+								}
+							};
+
+						return old;
+					}
+				);
+			}
+
+			return { previousBoard: board, data };
+		},
+		onSettled: (data) => {
 			queryClient.invalidateQueries(['board', { id: data?._id }]);
 		},
-		onError: () => {
+		onSuccess: () => {},
+		onError: (data, variables, context) => {
+			queryClient.setQueryData(
+				['board', { id: variables.boardId }],
+				(context as { previousBoard: BoardType }).previousBoard
+			);
 			setToastState({
 				open: true,
 				content: 'Error updating the comment',
