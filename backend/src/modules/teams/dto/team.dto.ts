e5f5efac5050e085fae9243405d481ac64ec10ac
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2022-07-20 09:06:20 +0100

    feat(swagger): add decorators to team dtos

--- backend/src/modules/teams/dto/team.dto.ts
+++ backend/src/modules/teams/dto/team.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsMongoId, IsNotEmpty, IsOptional, IsString, Validate } from 'class-validator';
 
 import { CheckUniqueUsers } from 'libs/validators/check-unique-users';
@@ -5,14 +6,17 @@ import { CheckUniqueUsers } from 'libs/validators/check-unique-users';
 import TeamUserDto from './team.user.dto';
 
 export default class TeamDto {
+	@ApiPropertyOptional()
 	@IsOptional()
 	@IsMongoId()
 	_id?: string;
 
+	@ApiProperty()
 	@IsString()
 	@IsNotEmpty()
 	name!: string;
 
+	@ApiProperty({ type: TeamUserDto, isArray: true })
 	@IsNotEmpty()
 	@Validate(CheckUniqueUsers)
 	users!: TeamUserDto[];
--- backend/src/modules/teams/dto/team.user.dto.ts
+++ backend/src/modules/teams/dto/team.user.dto.ts
@@ -1,18 +1,22 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEnum, IsMongoId, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 import { TeamRoles } from 'libs/enum/team.roles';
 
 export default class TeamUserDto {
+	@ApiProperty()
 	@IsString()
 	@IsNotEmpty()
 	@IsMongoId()
 	user!: string;
 
+	@ApiProperty({ type: String, enum: TeamRoles, enumName: 'Roles' })
 	@IsString()
 	@IsNotEmpty()
 	@IsEnum(TeamRoles, { each: true })
 	role!: string;
 
+	@ApiPropertyOptional()
 	@IsOptional()
 	@IsString()
 	@IsMongoId()
