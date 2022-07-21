20647f17e7c70cfb2dfe4a657fd04016ff6b59b0
Luís Francisco <67462841+dvpfran@users.noreply.github.com>
2022-07-21 15:48:56 +0100

    [FEATURE]: Optimistic Updates (#335)
    * feat: add optimistic updates on votes
    
    * fix: remove any type
    
    * [FEATURE]: Optimistic Updates - Cards (#333)
    
    * feat: optimistic cards
    
    * fix: code refactor
    
    * feat: replace onSuccess to onSettled function
    
    * fix: index of merge and unmerge cards
    
    * [FEATURE]: Optimistic Updates - Comments (#334)
    
    * feat: optimistic comments
    
    * fix: pr conflit
    
    * fix: update optimisc votes for merged cards as well
    
    * feat: reorganize functions
    
    * feat: remove unused code
    
    * chore: remove userId
    
    * fix: unmerge cards without votes error
    
    Co-authored-by: Geomar Bastiani <g.bastiani@kigroup.de>
    Co-authored-by: João Santos <68588265+joaosantos99@users.noreply.github.com>
    Co-authored-by: joaosantos99 <jfss291@gmail.com>

--- frontend/src/hooks/useComments.tsx
+++ frontend/src/hooks/useComments.tsx
@@ -1,43 +1,119 @@
 import { useMutation } from 'react-query';
+import { useSession } from 'next-auth/react';
 
+import {
+	handleAddComments,
+	handleDeleteComments,
+	handleUpdateComments
+} from 'helper/board/transformBoard';
+import BoardType from 'types/board/board';
+import ColumnType from 'types/column';
 import { addCommentRequest, deleteCommentRequest, updateCommentRequest } from '../api/boardService';
 import { ToastStateEnum } from '../utils/enums/toast-types';
 import useBoardUtils from './useBoardUtils';
 
 const useComments = () => {
 	const { queryClient, setToastState } = useBoardUtils();
+	const { data: session } = useSession({ required: false });
+
+	const getBoardQuery = (id: string | undefined) => ['board', { id }];
+
+	const setPreviousBoardQuery = (id: string, context: any) => {
+		queryClient.setQueryData(
+			getBoardQuery(id),
+			(context as { previousBoard: BoardType }).previousBoard
+		);
+	};
+
+	const getPrevData = async (id: string | undefined): Promise<BoardType | undefined> => {
+		const query = getBoardQuery(id);
+		await queryClient.cancelQueries(query);
+		const prevData = queryClient.getQueryData<{ board: BoardType }>(query);
+		return prevData?.board;
+	};
+
+	const updateBoardColumns = (id: string, columns: ColumnType[]) => {
+		queryClient.setQueryData<{ board: BoardType } | undefined>(
+			getBoardQuery(id),
+			(old: { board: BoardType } | undefined) => {
+				if (old)
+					return {
+						board: {
+							...old.board,
+							columns
+						}
+					};
+
+				return old;
+			}
+		);
+	};
 
 	const addCommentInCard = useMutation(addCommentRequest, {
-		onSuccess: (data) => {
+		onMutate: async (data) => {
+			const user = session?.user;
+			const board = await getPrevData(data.boardId);
+
+			if (board && user) {
+				const boardData = handleAddComments(board, data, user);
+				updateBoardColumns(data.boardId, boardData.columns);
+			}
+
+			return { previousBoard: board, data };
+		},
+		onSettled: (data) => {
 			queryClient.invalidateQueries(['board', { id: data?._id }]);
 		},
-		onError: () => {
+		onError: (data, variables, context) => {
+			setPreviousBoardQuery(variables.boardId, context);
 			setToastState({
 				open: true,
-				content: 'Error adding the comment',
+				content: 'Error updating the comment',
 				type: ToastStateEnum.ERROR
 			});
 		}
 	});
 
 	const deleteComment = useMutation(deleteCommentRequest, {
-		onSuccess: (data) => {
+		onMutate: async (data) => {
+			const board = await getPrevData(data.boardId);
+
+			if (board) {
+				const boardData = handleDeleteComments(board, data);
+				updateBoardColumns(data.boardId, boardData.columns);
+			}
+
+			return { previousBoard: board, data };
+		},
+		onSettled: (data) => {
 			queryClient.invalidateQueries(['board', { id: data?._id }]);
 		},
-		onError: () => {
+		onError: (data, variables, context) => {
+			setPreviousBoardQuery(variables.boardId, context);
 			setToastState({
 				open: true,
-				content: 'Error deleting the comment',
+				content: 'Error updating the comment',
 				type: ToastStateEnum.ERROR
 			});
 		}
 	});
 
 	const updateComment = useMutation(updateCommentRequest, {
-		onSuccess: (data) => {
+		onMutate: async (data) => {
+			const board = await getPrevData(data.boardId);
+
+			if (board) {
+				const boardData = handleUpdateComments(board, data);
+				updateBoardColumns(data.boardId, boardData.columns);
+			}
+
+			return { previousBoard: board, data };
+		},
+		onSettled: (data) => {
 			queryClient.invalidateQueries(['board', { id: data?._id }]);
 		},
-		onError: () => {
+		onError: (data, variables, context) => {
+			setPreviousBoardQuery(variables.boardId, context);
 			setToastState({
 				open: true,
 				content: 'Error updating the comment',
--- frontend/src/hooks/useVotes.tsx
+++ frontend/src/hooks/useVotes.tsx
@@ -2,57 +2,257 @@ import { useMutation } from 'react-query';
 import { useSession } from 'next-auth/react';
 
 import { addVoteRequest, deleteVoteRequest } from 'api/boardService';
+import { CardItemType } from 'types/card/cardItem';
+import voteDto from 'types/vote/vote.dto';
 import { ToastStateEnum } from 'utils/enums/toast-types';
 import isEmpty from 'utils/isEmpty';
+import BoardType from '../types/board/board';
 import { getRemainingVotes } from '../utils/getRemainingVotes';
 import useBoardUtils from './useBoardUtils';
+
+enum Action {
+	Add = 'add',
+	Remove = 'remove'
+}
+
+type QueryKeyType = (string | { id: string })[];
+
+type ToastStateType = {
+	open: boolean;
+	type: ToastStateEnum;
+	content: string;
+};
 
 const useVotes = () => {
 	const { queryClient, setToastState } = useBoardUtils();
 	const { data: session } = useSession({ required: true });
-	const userId = session?.user?.id;
+	const userId = session?.user?.id || '';
+
+	// work around to avoid read only error
+	const getEditableBoardData = (board: BoardType): BoardType => JSON.parse(JSON.stringify(board));
+
+	const getBoardQueryKey = (boardId = ''): QueryKeyType => ['board', { id: boardId }];
+
+	const getFirstCardItemIndexWithVotes = (cardItems: CardItemType[]) =>
+		cardItems.findIndex((cardItem) => cardItem.votes.length > 0);
+
+	const getBoardDataQuery = (boardQueryKey: QueryKeyType): BoardType =>
+		(queryClient.getQueryData(boardQueryKey) as { board: BoardType }).board;
+
+	const getPreviousBoardData = (boardQueryKey: QueryKeyType) =>
+		getEditableBoardData(getBoardDataQuery(boardQueryKey));
+
+	const shallAddVote = (action: Action) => action === Action.Add;
+
+	const hasMaxVotesLimit = ({ maxVotes }: BoardType) => !isEmpty(maxVotes);
+
+	const addVoteToCardItemOptimistic = (
+		prevBoardData: BoardType,
+		indexes: number[]
+	): BoardType => {
+		const newBoardData = prevBoardData;
+		const [colIndex, cardIndex, cardItemIndex] = indexes;
+
+		newBoardData.columns[colIndex].cards[cardIndex].items[cardItemIndex].votes.push(userId);
+
+		return newBoardData;
+	};
+
+	const removeVoteFromCardItemOptimistic = (prevBoardData: BoardType, indexes: number[]) => {
+		const newBoardData = prevBoardData;
+		const [colIndex, cardIndex, cardItemIndex] = indexes;
+
+		prevBoardData.columns[colIndex].cards[cardIndex].items[cardItemIndex].votes.pop();
+
+		return newBoardData;
+	};
+
+	const updateCardItemVoteOptimistic = (
+		prevBoardData: BoardType,
+		indexes: number[],
+		action: Action
+	) => {
+		if (shallAddVote(action)) return addVoteToCardItemOptimistic(prevBoardData, indexes);
+
+		return removeVoteFromCardItemOptimistic(prevBoardData, indexes);
+	};
+
+	const addVoteToCardsOptimistic = (
+		prevBoardData: BoardType,
+		indexes: number[],
+		hasVotesOnCards: boolean
+	): BoardType => {
+		const newBoardData = prevBoardData;
+		const [colIndex, cardIndex] = indexes;
+
+		if (hasVotesOnCards) {
+			newBoardData.columns[colIndex].cards[cardIndex].votes.push(userId);
+		} else {
+			newBoardData.columns[colIndex].cards[cardIndex].votes = [userId];
+		}
+
+		return newBoardData;
+	};
+
+	const removeVoteFromCardsOptimistic = (
+		prevBoardData: BoardType,
+		indexes: number[],
+		hasVotesOnCards: boolean
+	) => {
+		const newBoardData = prevBoardData;
+		const [colIndex, cardIndex] = indexes;
+
+		if (hasVotesOnCards) {
+			newBoardData.columns[colIndex].cards[cardIndex].votes.pop();
+
+			return newBoardData;
+		}
+
+		const cardItems = newBoardData.columns[colIndex].cards[cardIndex].items;
+		const cardItemIndex = getFirstCardItemIndexWithVotes(cardItems);
+		const newIndexes = [colIndex, cardIndex, cardItemIndex];
+
+		return updateCardItemVoteOptimistic(prevBoardData, newIndexes, Action.Remove);
+	};
+
+	const updateCardsVotesOptimistic = (
+		prevBoardData: BoardType,
+		indexes: number[],
+		action: Action
+	) => {
+		const [colIndex, cardIndex] = indexes;
+		const { votes: cardVotes } = prevBoardData.columns[colIndex].cards[cardIndex];
+		const hasVotesOnCards = cardVotes && cardVotes.length > 0;
+
+		if (shallAddVote(action)) {
+			return addVoteToCardsOptimistic(prevBoardData, indexes, hasVotesOnCards);
+		}
+
+		return removeVoteFromCardsOptimistic(prevBoardData, indexes, hasVotesOnCards);
+	};
+
+	const updateCardOrCardIndexVotesOptimistic = (
+		prevBoardData: BoardType,
+		indexes: number[],
+		isCardGroup: boolean,
+		action: Action
+	) => {
+		if (isCardGroup) return updateCardsVotesOptimistic(prevBoardData, indexes, action);
+
+		return updateCardItemVoteOptimistic(prevBoardData, indexes, action);
+	};
+
+	const updateBoardDataOptimistic = (
+		prevBoardData: BoardType,
+		voteData: voteDto,
+		action: Action
+	) => {
+		const { cardId, cardItemId, isCardGroup } = voteData;
+
+		const [colIndex, cardIndex, cardItemIndex] = [-1, -1, -1];
+		let indexes = [colIndex, cardIndex, cardItemIndex];
+
+		const foundCardItem = prevBoardData.columns.some((column, indexCol) =>
+			column.cards.some(
+				(card, indexCard) =>
+					card._id === cardId &&
+					card.items.some((cardItem, indexCardItem) => {
+						const cardItemFound = isCardGroup || cardItem._id === cardItemId;
+
+						if (cardItemFound) indexes = [indexCol, indexCard, indexCardItem];
+
+						return cardItemFound;
+					})
+			)
+		);
+
+		if (foundCardItem) {
+			return updateCardOrCardIndexVotesOptimistic(
+				prevBoardData,
+				indexes,
+				isCardGroup,
+				action
+			);
+		}
+
+		return prevBoardData;
+	};
+
+	const updateVoteOptimistic = async (action: Action, voteData: voteDto) => {
+		const boardQueryKey = getBoardQueryKey(voteData.boardId);
+
+		await queryClient.cancelQueries(boardQueryKey);
+
+		const prevBoardData: BoardType = getPreviousBoardData(boardQueryKey);
+
+		const newBoardData = updateBoardDataOptimistic(prevBoardData, voteData, action);
+
+		queryClient.setQueryData(boardQueryKey, { board: newBoardData });
+
+		return { newBoardData, prevBoardData };
+	};
+
+	const addVoteOptimistic = async (voteData: voteDto) =>
+		updateVoteOptimistic(Action.Add, voteData);
+
+	const removeVoteOptimistic = async (voteData: voteDto) =>
+		updateVoteOptimistic(Action.Remove, voteData);
+
+	const buildToastMessage = (
+		toastMessage: string,
+		toastStateType: ToastStateEnum
+	): ToastStateType => ({ open: true, content: toastMessage, type: toastStateType });
+
+	const toastErrorMessage = (errorMessage: string) =>
+		setToastState(buildToastMessage(errorMessage, ToastStateEnum.ERROR));
+
+	const toastInfoMessage = (toastMessage: string) =>
+		setToastState(buildToastMessage(toastMessage, ToastStateEnum.INFO));
+
+	const toastRemainingVotesMessage = (
+		message: string,
+		boardDataFromApi: BoardType | undefined
+	) => {
+		if (boardDataFromApi && hasMaxVotesLimit(boardDataFromApi)) {
+			const remainingVotes = getRemainingVotes(boardDataFromApi, userId!);
+
+			toastInfoMessage(`${message} You have ${remainingVotes} votes left.`);
+		}
+	};
+
+	const restoreBoardDataAndToastError = (
+		prevBoardData: BoardType | undefined,
+		{ boardId }: voteDto,
+		errorMessage: string
+	) => {
+		queryClient.setQueryData(getBoardQueryKey(boardId), prevBoardData);
+
+		toastErrorMessage(errorMessage);
+	};
+
+	const invalidateQueriesAndToastMessage = (
+		boardDataFromApi: BoardType | undefined,
+		message: string
+	) => {
+		queryClient.invalidateQueries(getBoardQueryKey(boardDataFromApi?._id));
+
+		toastRemainingVotesMessage(message, boardDataFromApi);
+	};
 
 	const addVote = useMutation(addVoteRequest, {
-		onSuccess: (data) => {
-			queryClient.invalidateQueries(['board', { id: data?._id }]);
-			if (!isEmpty(data.maxVotes)) {
-				setToastState({
-					open: true,
-					content: `You have ${getRemainingVotes(data, userId!)} votes left`,
-					type: ToastStateEnum.INFO
-				});
-			}
-		},
-		onError: () => {
-			setToastState({
-				open: true,
-				content: 'Error adding the vote',
-				type: ToastStateEnum.ERROR
-			});
-		}
+		onMutate: async (voteData) => addVoteOptimistic(voteData),
+		onSettled: (boardDataFromApi) =>
+			invalidateQueriesAndToastMessage(boardDataFromApi, 'Vote added.'),
+		onError: (_err, voteData, ctx) =>
+			restoreBoardDataAndToastError(ctx?.prevBoardData, voteData, 'Error adding the vote')
 	});
 
 	const deleteVote = useMutation(deleteVoteRequest, {
-		onSuccess: (data) => {
-			queryClient.invalidateQueries(['board', { id: data?._id }]);
-			if (!isEmpty(data.maxVotes)) {
-				setToastState({
-					open: true,
-					content: `Vote removed. You have ${getRemainingVotes(
-						data,
-						userId!
-					)} votes left.`,
-					type: ToastStateEnum.INFO
-				});
-			}
-		},
-		onError: () => {
-			setToastState({
-				open: true,
-				content: 'Error deleting the vote',
-				type: ToastStateEnum.ERROR
-			});
-		}
+		onMutate: async (voteData) => removeVoteOptimistic(voteData),
+		onSettled: (boardDataFromApi) =>
+			invalidateQueriesAndToastMessage(boardDataFromApi, 'Vote removed.'),
+		onError: (_err, voteData, ctx) =>
+			restoreBoardDataAndToastError(ctx?.prevBoardData, voteData, 'Error deleting the vote')
 	});
 
 	return {
