c1a6b8611a66266031e91f4fc952996c682fa7a8
Daniel Sousa <d.sousa@kigroup.de>
2022-07-22 08:45:33 +0100

    feat(swagger): azure endpoints

--- backend/src/modules/azure/controller/azure.controller.ts
+++ backend/src/modules/azure/controller/azure.controller.ts
@@ -1,7 +1,17 @@
 import { Body, Controller, Head, Inject, Param, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+	ApiBadRequestResponse,
+	ApiBody,
+	ApiInternalServerErrorResponse,
+	ApiOperation,
+	ApiParam,
+	ApiTags
+} from '@nestjs/swagger';
 
 import { EmailParam } from 'libs/dto/param/email.param';
+import { BadRequest } from 'libs/swagger/errors/bard-request.swagger';
+import { InternalServerError } from 'libs/swagger/errors/internal-server-error.swagger';
+import AzureLoginDto from 'modules/azure/swagger/dto/azure-login.dto';
 import { GetUserApplication } from 'modules/users/interfaces/applications/get.user.application.interface';
 import * as User from 'modules/users/interfaces/types';
 
@@ -9,7 +19,7 @@ import { AuthAzureApplication } from '../interfaces/applications/auth.azure.appl
 import { AzureToken } from '../interfaces/token.azure.dto';
 import { TYPES } from '../interfaces/types';
 
-@ApiTags('Authentication')
+@ApiTags('Azure')
 @Controller('auth/azure')
 export default class AzureController {
 	constructor(
@@ -19,11 +29,34 @@ export default class AzureController {
 		private getUserApp: GetUserApplication
 	) {}
 
+	@ApiOperation({ summary: 'Login or register the user with Azure' })
+	@ApiBody({
+		type: AzureLoginDto,
+		required: true
+	})
+	@ApiBadRequestResponse({
+		description: 'Bad Request',
+		type: BadRequest
+	})
+	@ApiInternalServerErrorResponse({
+		description: 'Internal Server Error',
+		type: InternalServerError
+	})
 	@Post('/')
 	loginOrRegisterAzureToken(@Body() azureToken: AzureToken) {
 		return this.authAzureApp.registerOrLogin(azureToken.token);
 	}
 
+	@ApiOperation({ summary: 'Validate if user exists on Azure Active Directory' })
+	@ApiParam({ name: 'email', type: String, format: 'email' })
+	@ApiBadRequestResponse({
+		description: 'Bad Request',
+		type: BadRequest
+	})
+	@ApiInternalServerErrorResponse({
+		description: 'Internal Server Error',
+		type: InternalServerError
+	})
 	@Head('user/:email')
 	async checkEmail(@Param() { email }: EmailParam) {
 		const existUserInAzure = await this.authAzureApp.checkUserExistsInActiveDirectory(email);
