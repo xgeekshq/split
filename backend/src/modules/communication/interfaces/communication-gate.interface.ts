9b7a477e0e2c9441dab841523f693890dc10cf6b
mourabraz <cmourabraz@gmail.com>
2022-07-23 23:57:08 +0100

    refactor: reorganize imports

--- backend/src/modules/communication/interfaces/communication-gate.interface.ts
+++ backend/src/modules/communication/interfaces/communication-gate.interface.ts
@@ -1,15 +1,15 @@
 export interface CommunicationGateInterface {
-  // channels
-  addChannel(name: string): Promise<{ id: string; name: string }>;
+	// channels
+	addChannel(name: string): Promise<{ id: string; name: string }>;
 
-  // users
-  addUsersToChannel(
-    channelId: string,
-    usersIds: string[],
-  ): Promise<{ ok: boolean; fails?: string[] }>;
-  getAllUsersByChannel(channelId: string): Promise<string[]>;
-  getEmailByUserId(userId: string): Promise<string>;
+	// users
+	addUsersToChannel(
+		channelId: string,
+		usersIds: string[]
+	): Promise<{ ok: boolean; fails?: string[] }>;
+	getAllUsersByChannel(channelId: string): Promise<string[]>;
+	getEmailByUserId(userId: string): Promise<string>;
 
-  // messages
-  addMessageToChannel(channelId: string, message: string): Promise<boolean>;
+	// messages
+	addMessageToChannel(channelId: string, message: string): Promise<boolean>;
 }
