d3a9e1bde54ca08dfb66b8330db13ad8131555f3
mourabraz <cmourabraz@gmail.com>
2022-08-02 14:44:12 +0100

    refactor: use assign by destructuring

--- backend/src/modules/communication/consumers/slack-consumer.processor.ts
+++ backend/src/modules/communication/consumers/slack-consumer.processor.ts
@@ -3,7 +3,7 @@ import { DoneCallback, Job } from 'bull';
 
 import { SlackCommunicationGateAdapter } from 'modules/communication/adapters/slack-communication-gate.adapter';
 import { SlackExecuteCommunication } from 'modules/communication/applications/slack-execute-communication.application';
-import { ConfigurationType, JobType } from 'modules/communication/dto/types';
+import { JobType } from 'modules/communication/dto/types';
 import { ChatSlackHandler } from 'modules/communication/handlers/chat-slack.handler';
 import { ConversationsSlackHandler } from 'modules/communication/handlers/conversations-slack.handler';
 import { UsersSlackHandler } from 'modules/communication/handlers/users-slack.handler';
@@ -13,12 +13,10 @@ const logger = new Logger('CommunicationConsumerProcessor');
 // If any error occurs the error will propagate and will be captured on queue's events
 // it is not necessary use the "cb" to deal with the Errors
 export default async function consumer(job: Job<JobType>, cb: DoneCallback) {
-	const data = job.data;
-	const board = data.board;
-	const config: ConfigurationType = data.config;
+	const { board, config } = job.data;
 
 	logger.verbose(
-		`execute communication for board with id: "${data.board.id}" and Job id: "${job.id}" (pid ${process.pid})`
+		`execute communication for board with id: "${board.id}" and Job id: "${job.id}" (pid ${process.pid})`
 	);
 
 	const communicationGateAdapter = new SlackCommunicationGateAdapter(config);
