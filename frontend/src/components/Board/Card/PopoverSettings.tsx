72ea369f2f6245a0db1287ae6d93b72a628132c3
joaosantos99 <jfss291@gmail.com>
2022-08-24 13:46:09 +0100

    fix: add click cap to unmerge button

--- frontend/src/components/Board/Card/PopoverSettings.tsx
+++ frontend/src/components/Board/Card/PopoverSettings.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { styled } from 'styles/stitches/stitches.config';
 
@@ -27,8 +27,18 @@ const PopoverSettingsContent: React.FC<PopoverSettingsContentProps> = ({
 	setEditCard,
 	setDeleteCard
 }) => {
+	const [unmergeClickLock, setUnmergeClickLock] = useState(true);
+
 	PopoverSettingsContent.defaultProps = {
 		setDeleteCard: undefined
+	};
+
+	const handleUnmergeCard = () => {
+		if (unmergeClickLock) {
+			unmergeCard();
+		}
+
+		setUnmergeClickLock(false);
 	};
 
 	return (
@@ -40,7 +50,7 @@ const PopoverSettingsContent: React.FC<PopoverSettingsContentProps> = ({
 				</Text>
 			</PopoverItem>
 			{isItem && (
-				<PopoverItem align="center" gap="8" onClick={unmergeCard}>
+				<PopoverItem align="center" gap="8" onClick={handleUnmergeCard}>
 					<Icon name="arrow-long-right" />
 					<Text size="sm" weight="medium">
 						Unmerge card
