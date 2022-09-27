c3b5a5f10e34bfe38b15f2f751919b16743cff06
Nuno Caseiro <n.caseiro@kigroup.de>
2022-09-27 16:13:10 +0100

    refactor(socket): avoid multiple connections

--- frontend/src/hooks/useSocketIO.ts
+++ frontend/src/hooks/useSocketIO.ts
@@ -1,33 +1,32 @@
-import { useCallback, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useQueryClient } from 'react-query';
 import { io, Socket } from 'socket.io-client';
 
 import { NEXT_PUBLIC_BACKEND_URL } from 'utils/constants';
 
-export const useSocketIO = (boardId: string): [string | null, () => void] => {
+export const useSocketIO = (boardId: string): string | undefined => {
 	const queryClient = useQueryClient();
-	const [socket, setSocket] = useState<Socket | null>(() => {
+	const [socket, setSocket] = useState<Socket | null>(null);
+
+	useEffect(() => {
 		const newSocket: Socket = io(NEXT_PUBLIC_BACKEND_URL ?? 'http://127.0.0.1:3200', {
 			transports: ['polling']
 		});
 
 		newSocket.on('connect', () => {
 			newSocket.emit('join', { boardId });
+			setSocket(newSocket);
 		});
 
 		newSocket.on('updateAllBoard', () => {
 			queryClient.invalidateQueries(['board', { id: boardId }]);
 		});
 
-		return newSocket;
-	});
+		return () => {
+			newSocket.disconnect();
+			setSocket(null);
+		};
+	}, [boardId, queryClient, setSocket]);
 
-	const clean = useCallback(() => {
-		if (socket) {
-			socket.close();
-		}
-		setSocket(null);
-	}, [socket]);
-
-	return [socket?.id || null, clean];
+	return socket?.id;
 };
