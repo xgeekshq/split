427abcd8b3f46c827fc200d566e345589d02a06c
Cátia Antunes <c.antunes@kigroup.de>
2022-10-24 13:02:15 +0100

    feat: add schema to search member form

---
+++ frontend/src/schema/schemaAddMemberForm.ts
@@ -0,0 +1,9 @@
+import Joi from 'joi';
+
+const SchemaAddMemberForm = Joi.object({
+	search: Joi.string().optional().trim().max(30).messages({
+		'string.max': 'Maximum of 30 characters'
+	})
+});
+
+export default SchemaAddMemberForm;
