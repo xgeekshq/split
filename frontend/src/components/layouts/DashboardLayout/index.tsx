54b9d5a4c027ce91e1d61cd48e9631c2aaeb4ba3
Cátia Antunes <c.antunes@kigroup.de>
2022-11-16 12:33:00 +0000

    feat: add dashboard layout to team page

--- frontend/src/components/layouts/DashboardLayout/index.tsx
+++ frontend/src/components/layouts/DashboardLayout/index.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import Icon from 'components/icons/Icon';
 import Flex from 'components/Primitives/Flex';
 import Text from 'components/Primitives/Text';
+import TeamHeader from '../../Teams/Team/Header';
 import { AddNewBoardButton, ContentSection } from './styles';
 
 type DashboardLayoutProps = {
@@ -12,38 +13,50 @@ type DashboardLayoutProps = {
 	isDashboard: boolean;
 	isBoards: boolean;
 	isTeams: boolean;
+	isTeamPage: boolean;
 };
 
 const DashboardLayout = (props: DashboardLayoutProps) => {
-	const { children, firstName, isDashboard, isBoards, isTeams } = props;
+	const { children, firstName, isDashboard, isBoards, isTeams, isTeamPage } = props;
 
 	return (
 		<ContentSection gap="36" justify="between">
-			<Flex css={{ width: '100%' }} direction="column" gap="40">
-				<Flex justify="between">
-					{isDashboard && <Text heading="1">Welcome, {firstName}</Text>}
-					{isBoards && <Text heading="1">Boards</Text>}
-					{isTeams && <Text heading="1">Teams</Text>}
-					{(isDashboard || isBoards) && (
-						<Link href="/boards/new">
-							<AddNewBoardButton size={isDashboard ? 'sm' : 'md'}>
-								<Icon css={{ color: 'white' }} name="plus" />
-								Add new board
-							</AddNewBoardButton>
-						</Link>
-					)}
-					{isTeams && (
-						<Link href="/teams/new">
-							<AddNewBoardButton size={isDashboard ? 'sm' : 'md'}>
-								<Icon css={{ color: 'white' }} name="plus" />
-								Create new team
-							</AddNewBoardButton>
-						</Link>
-					)}
+			{isDashboard || isTeams || isBoards ? (
+				<>
+					<Flex css={{ width: '100%' }} direction="column" gap="40">
+						<Flex justify="between">
+							{isDashboard && <Text heading="1">Welcome, {firstName}</Text>}
+							{isBoards && <Text heading="1">Boards</Text>}
+							{isTeams && <Text heading="1">Teams</Text>}
+							{(isDashboard || isBoards) && (
+								<Link href="/boards/new">
+									<AddNewBoardButton size={isDashboard ? 'sm' : 'md'}>
+										<Icon css={{ color: 'white' }} name="plus" />
+										Add new board
+									</AddNewBoardButton>
+								</Link>
+							)}
+							{isTeams && (
+								<Link href="/teams/new">
+									<AddNewBoardButton size={isDashboard ? 'sm' : 'md'}>
+										<Icon css={{ color: 'white' }} name="plus" />
+										Create new team
+									</AddNewBoardButton>
+								</Link>
+							)}
+							{isTeamPage && <TeamHeader title="Dev Split" />}
+						</Flex>
+						{children}
+					</Flex>
+					{/* {isDashboard && <CalendarBar />} */}
+				</>
+			) : null}
+			{isTeamPage && (
+				<Flex css={{ width: '100%' }} direction="column">
+					<Flex justify="between">{isTeamPage && <TeamHeader title="Dev Split" />}</Flex>
+					{children}
 				</Flex>
-				{children}
-			</Flex>
-			{/* {isDashboard && <CalendarBar />} */}
+			)}
 		</ContentSection>
 	);
 };
