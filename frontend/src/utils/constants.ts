733d4e62da18adeec5e6d3f3ec40376791228029
Rui Silva <r.silva@kigroup.de>
2022-11-22 17:21:34 +0000

    feat: remove nextjs standalone and inject env vars (#622)

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,20 +1,21 @@
-/* eslint-disable prefer-destructuring */
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig, serverRuntimeConfig } = getConfig();
+
 export const UNDEFINED = 'UNDEFINED';
 export const ERROR_LOADING_DATA = 'Error loading data';
 
-export const JWT_EXPIRATION_TIME = Number(process.env.NEXT_PUBLIC_EXPIRATION_TIME);
+export const JWT_EXPIRATION_TIME = Number(publicRuntimeConfig.NEXT_PUBLIC_EXPIRATION_TIME);
 
-export const NEXT_PUBLIC_BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
-export const BACKEND_URL = process.env.BACKEND_URL;
-export const SECRET = process.env.SECRET;
-export const NEXT_PUBLIC_NEXTAUTH_URL = process.env.NEXT_PUBLIC_NEXTAUTH_URL;
+export const { NEXT_PUBLIC_BACKEND_URL, NEXT_PUBLIC_NEXTAUTH_URL, SECRET } = publicRuntimeConfig;
 
-export const CLIENT_ID = process.env.AZURE_CLIENT_ID;
-export const CLIENT_SECRET = process.env.AZURE_CLIENT_SECRET;
-export const TENANT_ID = process.env.AZURE_TENANT_ID;
-export const NEXT_PUBLIC_ENABLE_AZURE = process.env.NEXT_PUBLIC_ENABLE_AZURE === 'true';
-export const NEXT_PUBLIC_ENABLE_GIT = process.env.NEXT_PUBLIC_ENABLE_GIT === 'true';
-export const NEXT_PUBLIC_ENABLE_GOOGLE = process.env.NEXT_PUBLIC_ENABLE_GOOGLE === 'true';
+export const CLIENT_ID = serverRuntimeConfig.AZURE_CLIENT_ID;
+export const CLIENT_SECRET = serverRuntimeConfig.AZURE_CLIENT_SECRET;
+export const TENANT_ID = serverRuntimeConfig.AZURE_TENANT_ID;
+
+export const NEXT_PUBLIC_ENABLE_AZURE = publicRuntimeConfig.NEXT_PUBLIC_ENABLE_AZURE === 'true';
+export const NEXT_PUBLIC_ENABLE_GIT = publicRuntimeConfig.NEXT_PUBLIC_ENABLE_GIT === 'true';
+export const NEXT_PUBLIC_ENABLE_GOOGLE = publicRuntimeConfig.NEXT_PUBLIC_ENABLE_GOOGLE === 'true';
 
 export const AUTH_SSO =
 	NEXT_PUBLIC_ENABLE_AZURE || NEXT_PUBLIC_ENABLE_GIT || NEXT_PUBLIC_ENABLE_GOOGLE;
