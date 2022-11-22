846ae6cf195ed079302391a08aa657871f47ecd8
Rui Silva <r.silva@kigroup.de>
2022-11-22 16:55:58 +0000

    feat: remove nextjs standalone and inject env vars

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,6 +8,20 @@ module.exports = {
 	reactStrictMode: true,
 	output: 'standalone',
 	swcMinify: true,
+	publicRuntimeConfig: {
+		NEXT_PUBLIC_EXPIRATION_TIME: process.env.NEXT_PUBLIC_EXPIRATION_TIME,
+		NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+		SECRET: process.env.SECRET,
+		NEXT_PUBLIC_NEXTAUTH_URL: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
+		NEXT_PUBLIC_ENABLE_AZURE: process.env.NEXT_PUBLIC_ENABLE_AZURE,
+		NEXT_PUBLIC_ENABLE_GIT: process.env.NEXT_PUBLIC_ENABLE_GIT,
+		NEXT_PUBLIC_ENABLE_GOOGLE: process.env.NEXT_PUBLIC_ENABLE_GOOGLE
+	},
+	serverRuntimeConfig: {
+		AZURE_CLIENT_ID: process.env.AZURE_CLIENT_ID,
+		AZURE_CLIENT_SECRET: process.env.AZURE_CLIENT_SECRET,
+		AZURE_TENANT_ID: process.env.AZURE_TENANT_ID
+	},
 	webpackDevMiddleware: (config) => {
 		config.watchOptions = {
 			poll: 1000,
