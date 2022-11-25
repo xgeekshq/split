08c5afde885858a18f850d6be035a80e3015f1f7
Patrícia Dias <patricia@Patricias-MBP.Home>
2022-11-25 10:04:39 +0000

    fix: removed unnecessary throws

--- backend/src/modules/teams/services/delete.team.service.ts
+++ backend/src/modules/teams/services/delete.team.service.ts
@@ -1,33 +1,20 @@
-import { forwardRef, Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { ClientSession, LeanDocument, Model } from 'mongoose';
+import { ClientSession, Model } from 'mongoose';
 
 import { DELETE_FAILED } from 'libs/exceptions/messages';
 
 import { DeleteTeamServiceInterface } from '../interfaces/services/delete.team.service.interface';
-import { GetTeamServiceInterface } from '../interfaces/services/get.team.service.interface';
-import * as Teams from '../interfaces/types';
 import Team, { TeamDocument } from '../schemas/teams.schema';
 
 @Injectable()
 export default class DeleteTeamService implements DeleteTeamServiceInterface {
-	constructor(
-		@InjectModel(Team.name) private teamModel: Model<TeamDocument>,
-		@Inject(forwardRef(() => Teams.TYPES.services.GetTeamService))
-		private getTeamService: GetTeamServiceInterface
-	) {}
+	constructor(@InjectModel(Team.name) private teamModel: Model<TeamDocument>) {}
 
 	async delete(teamId: string): Promise<boolean> {
-		const team = await this.getTeam(teamId);
-		// no need ?
-		if (!team) {
-			throw new NotFoundException('Team not found!');
-		}
-
 		const teamSession = await this.teamModel.db.startSession();
 		teamSession.startTransaction();
 		try {
-			// delete team
 			await this.deleteTeam(teamId, teamSession);
 			await teamSession.commitTransaction();
 			return true;
@@ -40,34 +27,6 @@ export default class DeleteTeamService implements DeleteTeamServiceInterface {
 		return false;
 	}
 
-	/* ----- HELPERS ------ */
-	private async getTeam(teamId: string): Promise<LeanDocument<TeamDocument>> {
-		const team = await this.getTeamService.getTeam(teamId);
-		if (!team) {
-			throw new NotFoundException('Team not found!');
-		}
-		return team;
-	}
-
-	// private async getUser(userId: string): Promise<LeanDocument<UserDocument>> {
-	// 	const user = await this.getUserService.getById(userId);
-	// 	if (!user) {
-	// 		throw new NotFoundException('User not found!');
-	// 	}
-	// 	return user;
-	// }
-
-	// private async getTeamUser(
-	// 	userId: string,
-	// 	teamId: string
-	// ): Promise<LeanDocument<TeamUserDocument>> {
-	// 	const teamUser = await this.getTeamService.getTeamUser(userId, teamId);
-	// 	if (!teamUser) {
-	// 		throw new NotFoundException('User does not belong to team!');
-	// 	}
-	// 	return teamUser;
-	// }
-
 	private async deleteTeam(teamId: string, teamSession: ClientSession) {
 		const result = await this.teamModel.findOneAndRemove(
 			{
@@ -76,7 +35,7 @@ export default class DeleteTeamService implements DeleteTeamServiceInterface {
 			{ session: teamSession }
 		);
 
-		if (!result) throw Error(DELETE_FAILED);
+		if (!result) throw new NotFoundException(DELETE_FAILED);
 		return { _id: result._id };
 	}
 }
