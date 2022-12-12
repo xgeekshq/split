3911373b0ea644f8ab5d10673fcf3e61949796d8
Patrícia Dias <p.dias@kigroup.de>
2022-12-12 10:01:47 +0000

    feat: endpoint retrieves list of created team users

--- backend/src/modules/teams/interfaces/services/update.team.service.interface.ts
+++ backend/src/modules/teams/interfaces/services/update.team.service.interface.ts
@@ -4,5 +4,8 @@ import { TeamUserDocument } from '../../schemas/team.user.schema';
 
 export interface UpdateTeamServiceInterface {
 	updateTeamUser(teamData: TeamUserDto): Promise<LeanDocument<TeamUserDocument> | null>;
-	addAndRemoveTeamUsers(addUsers: TeamUserDto[], removeUsers: string[]): Promise<boolean>;
+	addAndRemoveTeamUsers(
+		addUsers: TeamUserDto[],
+		removeUsers: string[]
+	): Promise<LeanDocument<TeamUserDocument[]>>;
 }
