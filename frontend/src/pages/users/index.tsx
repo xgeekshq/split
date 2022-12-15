c3bac988f2683072982d792d60573a30d1c83a39
Rafael Batista <118206043+RafaelSBatista97@users.noreply.github.com>
2022-12-15 11:53:55 +0000

    feat: users lazy loading scroll (#683)

--- frontend/src/pages/users/index.tsx
+++ frontend/src/pages/users/index.tsx
@@ -1,47 +1,22 @@
 import { ReactElement, Suspense } from 'react';
-import { dehydrate, QueryClient, useQuery } from 'react-query';
-import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import { useSession } from 'next-auth/react';
-import { useSetRecoilState } from 'recoil';
 
 import QueryError from '@/components/Errors/QueryError';
 import Layout from '@/components/layouts/Layout';
 import LoadingPage from '@/components/loadings/LoadingPage';
 import Flex from '@/components/Primitives/Flex';
 import UsersList from '@/components/Users/UsersList';
-import { getAllUsersWithTeams } from '../../api/userService';
-import requireAuthentication from '../../components/HOC/requireAuthentication';
-import { toastState } from '../../store/toast/atom/toast.atom';
-import { usersWithTeamsState } from '../../store/user/atoms/user.atom';
-import { ToastStateEnum } from '../../utils/enums/toast-types';
 
 const Users = () => {
   const { data: session } = useSession({ required: true });
-  const setToastState = useSetRecoilState(toastState);
-  const setUsersWithTeamsState = useSetRecoilState(usersWithTeamsState);
 
-  const { data, isFetching } = useQuery(['usersWithTeams'], () => getAllUsersWithTeams(), {
-    enabled: false,
-    refetchOnWindowFocus: false,
-    onError: () => {
-      setToastState({
-        open: true,
-        content: 'Error getting the users',
-        type: ToastStateEnum.ERROR,
-      });
-    },
-  });
-
-  if (data) {
-    setUsersWithTeamsState(data);
-  }
-  if (!session || !data) return null;
+  if (!session) return null;
 
   return (
     <Flex direction="column">
       <Suspense fallback={<LoadingPage />}>
         <QueryError>
-          <UsersList isFetching={isFetching} />
+          <UsersList />
         </QueryError>
       </Suspense>
     </Flex>
@@ -51,16 +26,3 @@ const Users = () => {
 Users.getLayout = (page: ReactElement) => <Layout>{page}</Layout>;
 
 export default Users;
-
-export const getServerSideProps: GetServerSideProps = requireAuthentication(
-  async (context: GetServerSidePropsContext) => {
-    const queryClient = new QueryClient();
-    await queryClient.prefetchQuery('usersWithTeams', () => getAllUsersWithTeams(context));
-
-    return {
-      props: {
-        dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
-      },
-    };
-  },
-);
