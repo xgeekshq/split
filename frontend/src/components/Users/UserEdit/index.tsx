770f56de3807cc30604b1b5a7a3f9b36bc7a8cd6
Rafael Batista <118206043+RafaelSBatista97@users.noreply.github.com>
2022-12-20 11:49:09 +0000

    feat: top layout for users edit page (#718)

---
+++ frontend/src/components/Users/UserEdit/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import ListOfCards from './partials/ListOfCards';
+
+const UsersEdit = () => <ListOfCards />;
+
+export default UsersEdit;
--- frontend/src/pages/users/[userId].tsx
+++ frontend/src/pages/users/[userId].tsx
@@ -1,3 +1,37 @@
-const UserDetails = () => <h1>User Details</h1>;
+import { ReactElement, Suspense } from 'react';
+import { useSession } from 'next-auth/react';
+
+import QueryError from '@/components/Errors/QueryError';
+import Layout from '@/components/layouts/Layout';
+import LoadingPage from '@/components/loadings/LoadingPage';
+import Flex from '@/components/Primitives/Flex';
+import UsersEdit from '@/components/Users/UserEdit';
+import { ContentSection } from '@/components/layouts/DashboardLayout/styles';
+import UserHeader from '@/components/Users/UserEdit/partials/UserHeader';
+
+const UserDetails = () => {
+  const { data: session } = useSession({ required: true });
+
+  if (!session) return null;
+
+  return (
+    <Flex direction="column">
+      <Suspense fallback={<LoadingPage />}>
+        <QueryError>
+          <ContentSection gap="36" justify="between">
+            <Flex css={{ width: '100%' }} direction="column">
+              <Flex justify="start">
+                <UserHeader title="Nuno Caseiro" />
+              </Flex>
+            </Flex>
+            <UsersEdit />
+          </ContentSection>
+        </QueryError>
+      </Suspense>
+    </Flex>
+  );
+};
+
+UserDetails.getLayout = (page: ReactElement) => <Layout>{page}</Layout>;
 
 export default UserDetails;
