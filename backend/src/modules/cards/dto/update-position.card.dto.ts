c2ff1d558e1e7d9bc1a108439666d553a6a3206a
Nuno Caseiro <n.caseiro@kigroup.de>
2023-01-08 15:13:53 +0000

    refactor: remove isString validator

--- backend/src/modules/cards/dto/update-position.card.dto.ts
+++ backend/src/modules/cards/dto/update-position.card.dto.ts
@@ -4,7 +4,6 @@ import { BaseDto } from 'src/libs/dto/base.dto';
 export class UpdateCardPositionDto extends BaseDto {
 	@IsNotEmpty()
 	@IsMongoId()
-	@IsString()
 	targetColumnId!: string;
 
 	@IsNotEmpty()
@@ -13,12 +12,10 @@ export class UpdateCardPositionDto extends BaseDto {
 
 	@IsNotEmpty()
 	@IsMongoId()
-	@IsString()
 	boardId!: string;
 
 	@IsNotEmpty()
 	@IsMongoId()
-	@IsString()
 	colIdOfCard: string;
 
 	@IsNotEmpty()
@@ -27,7 +24,6 @@ export class UpdateCardPositionDto extends BaseDto {
 
 	@IsNotEmpty()
 	@IsMongoId()
-	@IsString()
 	cardId: string;
 
 	@IsNotEmpty()
