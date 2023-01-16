47baad870e01fc0f1eaa0c39d8f847596793b9e6
Cátia Antunes <c.antunes@kigroup.de>
2023-01-16 10:45:17 +0000

    fix: add arrows and links to tiles

--- frontend/src/components/Dashboard/Tiles/index.tsx
+++ frontend/src/components/Dashboard/Tiles/index.tsx
@@ -1,6 +1,8 @@
 import Icon from '@/components/icons/Icon';
 import Text from '@/components/Primitives/Text';
 import { HeaderInfo } from '@/types/dashboard/header.info';
+import { BOARDS_ROUTE, TEAMS_ROUTE, USERS_ROUTE } from '@/utils/routes';
+import Link from 'next/link';
 import { GridContainer, StyledTile } from './styles';
 
 type TilesProps = {
@@ -9,58 +11,100 @@ type TilesProps = {
 
 const DashboardTiles = ({ data }: TilesProps) => (
   <GridContainer>
-    <StyledTile>
-      <Text color="white" size="md">
-        Your boards
-      </Text>
-      <h3>{data.boardsCount}</h3>
+    <Link href={`${BOARDS_ROUTE}`}>
+      <StyledTile>
+        <Text color="white" size="md">
+          Your boards
+        </Text>
+        <h3>{data.boardsCount}</h3>
 
-      <Icon
-        name="blob-purple"
-        css={{
-          position: 'absolute',
-          right: '-1px',
-          top: '0',
-          bottom: '-1px',
-          width: '$100',
-          height: '$100',
-        }}
-      />
-    </StyledTile>
-    <StyledTile>
-      <Text color="white" size="md">
-        Your teams
-      </Text>
-      <h3>{data?.teamsCount}</h3>
+        <Icon
+          name="blob-purple"
+          css={{
+            position: 'absolute',
+            right: '-1px',
+            top: '0',
+            bottom: '-1px',
+            width: '$100',
+            height: '$100',
+          }}
+        />
+        <Icon
+          name="arrow-long-right"
+          css={{
+            position: 'absolute',
+            right: '20px',
+            top: '50%',
+            bottom: '-1px',
+            width: '$24',
+            height: '$24',
+            color: '$black',
+          }}
+        />
+      </StyledTile>
+    </Link>
+    <Link href={`${TEAMS_ROUTE}`}>
+      <StyledTile>
+        <Text color="white" size="md">
+          Your teams
+        </Text>
+        <h3>{data?.teamsCount}</h3>
 
-      <Icon
-        name="blob-blue"
-        css={{
-          position: 'absolute',
-          right: '-1px',
-          top: '0px',
-          width: '126px',
-          height: '$90',
-        }}
-      />
-    </StyledTile>
-    <StyledTile>
-      <Text color="white" size="md">
-        Active Members
-      </Text>
-      <h3>{data?.usersCount}</h3>
+        <Icon
+          name="blob-blue"
+          css={{
+            position: 'absolute',
+            right: '-1px',
+            top: '0px',
+            width: '126px',
+            height: '$90',
+          }}
+        />
+        <Icon
+          name="arrow-long-right"
+          css={{
+            position: 'absolute',
+            right: '20px',
+            top: '50%',
+            bottom: '-1px',
+            width: '$24',
+            height: '$24',
+            color: '$black',
+          }}
+        />
+      </StyledTile>
+    </Link>
+    <Link href={`${USERS_ROUTE}`}>
+      <StyledTile>
+        <Text color="white" size="md">
+          Active Members
+        </Text>
+        <h3>{data?.usersCount}</h3>
 
-      <Icon
-        name="blob-yellow"
-        css={{
-          position: 'absolute',
-          right: '-1px',
-          bottom: '-1px',
-          width: '127px',
-          height: '76px',
-        }}
-      />
-    </StyledTile>
+        <Icon
+          name="blob-yellow"
+          css={{
+            position: 'absolute',
+            right: '-1px',
+            bottom: '-1px',
+            width: '127px',
+            height: '76px',
+          }}
+        />
+        <Icon
+          name="arrow-long-right"
+          css={{
+            position: 'absolute',
+            right: '20px',
+            top: '50%',
+            bottom: '-1px',
+            width: '$24',
+            height: '$24',
+            color: '$black',
+          }}
+        />
+      </StyledTile>
+    </Link>
   </GridContainer>
 );
 
