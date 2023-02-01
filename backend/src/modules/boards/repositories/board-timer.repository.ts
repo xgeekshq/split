824c43f53b1cf658769ce3d73083c2e797831d38
Geomar Bastiani <mail@geomarbastiani.com>
2023-02-01 22:37:53 +0000

    feat: add board timer repository

---
+++ backend/src/modules/boards/repositories/board-timer.repository.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@nestjs/common';
+import TimerHelper from 'src/modules/boards/helpers/timer.helper';
+
+type BoardTimer = {
+	boardId: string;
+	clientId: string;
+	timerHelper: TimerHelper;
+};
+
+@Injectable()
+export default class BoardTimerRepository {
+	boardTimers: BoardTimer[] = [];
+
+	getOrCreateBoardTimer(boardId: string, clientId: string): BoardTimer {
+		const boardTimerFound = this.findBoardTimerByBoardId(boardId);
+
+		if (boardTimerFound) {
+			return boardTimerFound;
+		}
+
+		const newBoardTimer = this.createTimer(boardId, clientId);
+
+		return newBoardTimer;
+	}
+
+	private createTimer(boardId: string, clientId: string) {
+		const timerHelper = new TimerHelper();
+		const newBoardTimer = { boardId, clientId, timerHelper };
+		this.boardTimers.push(newBoardTimer);
+
+		return newBoardTimer;
+	}
+
+	findBoardTimerByBoardId(boardId: string): BoardTimer | null {
+		const found = this.boardTimers.find((boardTimer) => boardTimer.boardId === boardId);
+
+		return found;
+	}
+
+	removeTimer(boardId: string): void {
+		const boardTimerIndex = this.boardTimers.findIndex(
+			(boardTimer) => boardTimer.boardId === boardId
+		);
+
+		if (boardTimerIndex > -1) {
+			this.boardTimers[boardTimerIndex].timerHelper.stop();
+			this.boardTimers.splice(boardTimerIndex, 1);
+		}
+	}
+}
