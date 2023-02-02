6e422effd6c6119477b9527fd51e7223fe970fcb
Geomar Bastiani <mail@geomarbastiani.com>
2023-02-02 17:38:45 +0000

    feat: move dtos from common to libs

--- backend/src/libs/dto/board-timer-state.dto.ts
+++ backend/src/libs/dto/board-timer-state.dto.ts
@@ -1,5 +1,5 @@
-import BoardTimerDto from 'src/modules/common/dtos/board-timer.dto';
-import TimeDto from 'src/modules/common/dtos/time.dto';
+import BoardTimerDto from 'src/libs/dto/board-timer.dto';
+import TimeDto from 'src/libs/dto/time.dto';
 import TimerStatusDto from 'src/modules/common/dtos/timer-status.dto';
 
 export default interface BoardTimerStateDto extends BoardTimerDto {
--- backend/src/modules/common/dtos/board-timer-time-left.dto.ts
+++
@@ -1,6 +0,0 @@
-import BoardTimerDto from 'src/modules/common/dtos/board-timer.dto';
-import TimeDto from 'src/modules/common/dtos/time.dto';
-
-export default interface BoardTimerTimeLeftDto extends BoardTimerDto {
-	timeLeft: TimeDto;
-}
