7c8cd2b4464f753b1850b371c12b81e1d5cd9aeb
João Salvador <24455614+JoaoSaIvador@users.noreply.github.com>
2023-02-07 12:33:10 +0000

    feat: storybook deployment (#1036)

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,30 +1,34 @@
 const path = require('path');
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
   },
-  "webpackFinal": async (config, { configType }) => {
-    config.resolve.modules = [
-      path.resolve(__dirname, ".."),
-      "node_modules",
-    ]
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules'];
 
     config.resolve.alias = {
       ...config.resolve.alias,
-      "@": path.resolve(__dirname, "../src")
+      '@': path.resolve(__dirname, '../src'),
     };
 
+    if (process.env.NODE_ENV === 'production') {
+      config.output.publicPath = '/storybook/';
+    }
+
     return config;
-  }
-}
+  },
+  managerWebpack: async (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.output.publicPath = '/storybook/';
+    }
+    return config;
+  },
+};
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,23 +2,31 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-	reactStrictMode: true,
-	output: 'standalone',
-	swcMinify: true,
-	publicRuntimeConfig: {
-		NEXT_PUBLIC_EXPIRATION_TIME: process.env.NEXT_PUBLIC_EXPIRATION_TIME,
-		NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
-		SECRET: process.env.SECRET,
-		NEXT_PUBLIC_NEXTAUTH_URL: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
-		NEXT_PUBLIC_ENABLE_AZURE: process.env.NEXT_PUBLIC_ENABLE_AZURE,
-		NEXT_PUBLIC_ENABLE_GIT: process.env.NEXT_PUBLIC_ENABLE_GIT,
-		NEXT_PUBLIC_ENABLE_GOOGLE: process.env.NEXT_PUBLIC_ENABLE_GOOGLE,
-		NEXT_PUBLIC_LOGIN_SSO_ONLY: process.env.NEXT_PUBLIC_LOGIN_SSO_ONLY,
-		NEXT_PUBLIC_REGULAR_BOARD: process.env.NEXT_PUBLIC_REGULAR_BOARD
-	},
-	serverRuntimeConfig: {
-		AZURE_CLIENT_ID: process.env.AZURE_CLIENT_ID,
-		AZURE_CLIENT_SECRET: process.env.AZURE_CLIENT_SECRET,
-		AZURE_TENANT_ID: process.env.AZURE_TENANT_ID
-	},
+  reactStrictMode: true,
+  output: 'standalone',
+  swcMinify: true,
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_EXPIRATION_TIME: process.env.NEXT_PUBLIC_EXPIRATION_TIME,
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    SECRET: process.env.SECRET,
+    NEXT_PUBLIC_NEXTAUTH_URL: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
+    NEXT_PUBLIC_ENABLE_AZURE: process.env.NEXT_PUBLIC_ENABLE_AZURE,
+    NEXT_PUBLIC_ENABLE_GIT: process.env.NEXT_PUBLIC_ENABLE_GIT,
+    NEXT_PUBLIC_ENABLE_GOOGLE: process.env.NEXT_PUBLIC_ENABLE_GOOGLE,
+    NEXT_PUBLIC_LOGIN_SSO_ONLY: process.env.NEXT_PUBLIC_LOGIN_SSO_ONLY,
+    NEXT_PUBLIC_REGULAR_BOARD: process.env.NEXT_PUBLIC_REGULAR_BOARD,
+  },
+  serverRuntimeConfig: {
+    AZURE_CLIENT_ID: process.env.AZURE_CLIENT_ID,
+    AZURE_CLIENT_SECRET: process.env.AZURE_CLIENT_SECRET,
+    AZURE_TENANT_ID: process.env.AZURE_TENANT_ID,
+  },
+  rewrites: async () => {
+    return [
+      {
+        source: '/storybook',
+        destination: '/storybook/index.html',
+      },
+    ];
+  },
 };
