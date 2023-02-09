319c098aea4ad37de7d54c3bf0fadb388c7e811b
Nuno Caseiro <n.caseiro@kigroup.de>
2023-02-09 18:39:13 +0000

    chore: add maze script

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,4 +1,6 @@
-import { ReactElement, ReactNode } from 'react';
+/* eslint-disable */
+// @ts-nocheck
+import { ReactElement, ReactNode, useEffect } from 'react';
 import { DehydratedState, Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { NextPage } from 'next';
@@ -12,11 +14,10 @@ import globalStyles from '@/styles/globals';
 
 import Sprite from '@/components/icons/Sprite';
 import Toast, { ToastProvider, ToastViewport } from '@/components/Primitives/Toast';
-import { JWT_EXPIRATION_TIME, RECOIL_DEV_TOOLS } from '@/utils/constants';
+import { JWT_EXPIRATION_TIME } from '@/utils/constants';
 import { ROUTES } from '@/utils/routes';
 import { Session } from 'next-auth';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import RecoilDevTools from '@/components/RecoilDevTools/RecoilDevTools';
 
 type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -43,6 +44,35 @@ function Root({
 
   const router = useRouter();
   globalStyles();
+
+  useEffect(() => {
+    (function (m, a, z, e) {
+      let s;
+      let t;
+      try {
+        t = m.sessionStorage.getItem('maze-us');
+      } catch (err) {}
+
+      if (!t) {
+        t = new Date().getTime();
+        try {
+          m.sessionStorage.setItem('maze-us', String(t));
+        } catch (err) {}
+      }
+
+      s = a.createElement('script');
+      s.src = z + '?t=' + t + '&apiKey=' + e;
+      s.async = true;
+      a.getElementsByTagName('head')[0].appendChild(s);
+      m.mazeUniversalSnippetApiKey = e;
+    })(
+      window,
+      document,
+      'https://snippet.maze.co/maze-universal-loader.js',
+      '3023cbc3-dd28-4294-9b9f-154ca2b3e7b0',
+    );
+  });
+
   return (
     <>
       <Head>
@@ -57,7 +87,6 @@ function Root({
               <RecoilRoot>
                 {getLayout(<Component {...pageProps} />)}
                 <Toast />
-                {RECOIL_DEV_TOOLS && <RecoilDevTools />}
               </RecoilRoot>
               <ToastViewport
                 css={{
