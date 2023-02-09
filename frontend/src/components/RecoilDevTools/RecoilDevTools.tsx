714e1b9ee043f4428c4d18721191b4270d3b6807
Guido <stereopt@gmail.com>
2023-02-09 16:41:18 +0000

    feat: basic recoil dev tools

---
+++ frontend/src/components/RecoilDevTools/RecoilDevTools.tsx
@@ -0,0 +1,39 @@
+import { useRecoilCallback } from 'recoil';
+import Icon from '../icons/Icon';
+import Button from '../Primitives/Button';
+
+const RecoilDevTools = () => {
+  const getAtomValues = async (snapshot: any) => {
+    // eslint-disable-next-line no-console
+    console.log('Atom Values:');
+    const state: any = {};
+    // eslint-disable-next-line no-restricted-syntax
+    for (const node of snapshot.getNodes_UNSTABLE()) {
+      // eslint-disable-next-line no-await-in-loop
+      const value = await snapshot.getPromise(node);
+      state[node.key] = value;
+    }
+    // eslint-disable-next-line no-console
+    console.log(state);
+  };
+
+  const dumpRecoilState = useRecoilCallback(
+    ({ snapshot }) =>
+      async () =>
+        getAtomValues(snapshot),
+    [],
+  );
+
+  return (
+    <Button
+      isIcon
+      css={{ position: 'fixed', bottom: '$58', left: '$12' }}
+      size="lg"
+      onClick={dumpRecoilState}
+    >
+      <Icon name="settings" css={{ color: '$secondaryDark' }} />
+    </Button>
+  );
+};
+
+export default RecoilDevTools;
