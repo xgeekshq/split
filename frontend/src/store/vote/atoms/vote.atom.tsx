d945593df41ac09f7552c4d2f3fb5e395195f870
Patrícia Dias <116013814+patricia-mdias@users.noreply.github.com>
2023-02-14 14:57:43 +0000

    fix: renamed keys of atoms (#1080)

--- frontend/src/store/vote/atoms/vote.atom.tsx
+++ frontend/src/store/vote/atoms/vote.atom.tsx
@@ -1,6 +1,6 @@
 import { atom } from 'recoil';
 
 export const maxVotesReachedAtom = atom<{ maxVotesReached: boolean; noVotes: boolean }>({
-  key: 'usersWithTeams',
+  key: 'maxVotesReachedAtom',
   default: { maxVotesReached: false, noVotes: false },
 });
