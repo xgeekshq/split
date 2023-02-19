a35938fa3fe7fcb51d0362c74e951b59d36e95ff
Nuno Caseiro <n.caseiro@kigroup.de>
2023-02-19 15:40:51 +0000

    fix: participants names remain in the text area

--- frontend/src/components/CreateBoard/RegularBoard/ParticipantsTab/SelectParticipants/index.tsx
+++ frontend/src/components/CreateBoard/RegularBoard/ParticipantsTab/SelectParticipants/index.tsx
@@ -33,12 +33,19 @@ const SelectParticipants = () => {
   useEffect(() => {
     const updateCheckedUser = usersList.map((user) => ({
       ...user,
-      isChecked: user._id === session?.user.id,
+      isChecked: user._id === session?.user.id || user.isChecked,
     }));
 
     const users = updateCheckedUser.flatMap((user) =>
-      user._id === session?.user.id
-        ? [{ role: BoardUserRoles.RESPONSIBLE, user: user._id, votesCount: 0 }]
+      user.isChecked
+        ? [
+            {
+              role:
+                user._id === session?.user.id ? BoardUserRoles.RESPONSIBLE : BoardUserRoles.MEMBER,
+              user: user._id,
+              votesCount: 0,
+            },
+          ]
         : [],
     );
     setUsersList(updateCheckedUser);
