38583b06112994126b4121cbbe12897252cfded5
JoaoSaIvador <j.salvador@kigroup.de>
2023-02-23 11:31:22 +0000

    test: add main page header tests

--- frontend/src/components/layouts/Layout/partials/MainPageHeader.tsx
+++ frontend/src/components/layouts/Layout/partials/MainPageHeader.tsx
@@ -4,13 +4,13 @@ import Link from 'next/link';
 import Button from '@/components/Primitives/Button';
 import Icon from '@/components/Primitives/Icon';
 
-type DashboardLayoutProps = {
+export type MainPagerHeaderProps = {
   title: string;
   button?: { link: string; label: string };
 };
 
-const MainPageHeader = ({ title, button }: DashboardLayoutProps) => (
-  <Flex justify="between">
+const MainPageHeader = ({ title, button }: MainPagerHeaderProps) => (
+  <Flex justify="between" data-testid="MainPageHeader">
     <Text heading="1">{title}</Text>
     {button && (
       <Link href={button.link ?? '#'}>
