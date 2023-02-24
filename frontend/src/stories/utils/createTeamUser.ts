df523a68c432ae6fcb0a75d05c3e8906e87982f8
Guido <stereopt@gmail.com>
2023-02-24 18:20:29 +0000

    fix: admin team user role

--- frontend/src/stories/utils/createTeamUser.ts
+++ frontend/src/stories/utils/createTeamUser.ts
@@ -1,10 +1,14 @@
 import { Team } from '@/types/team/team';
+import { TeamUserRoles } from '@/utils/enums/team.user.roles';
 import { TeamUserFactory } from '../../utils/factories/user';
 
 export const createTeamUser = (user: any, team: Team) => {
   const foundUser = team.users.findIndex((u) => String(u.user._id) === String(user.id));
   if (foundUser !== -1) return;
 
-  const teamUser = TeamUserFactory.create({ user: { _id: user.id, ...user } });
+  const teamUser = TeamUserFactory.create({
+    user: { _id: user.id, ...user },
+    role: TeamUserRoles.ADMIN,
+  });
   team.users.push(teamUser);
 };
