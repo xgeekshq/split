504b340f64315e60c44d058476fd1edd73d2693d
Nuno Caseiro <n.caseiro@kigroup.de>
2023-02-28 18:07:25 +0000

    fix: all boards merged message

--- backend/src/modules/communication/applications/slack-merge-board.application.ts
+++ backend/src/modules/communication/applications/slack-merge-board.application.ts
@@ -11,13 +11,11 @@ export class SlackMergeBoardApplication implements MergeBoardApplicationInterfac
 	async execute(data: MergeBoardType): Promise<MergeBoardType | null> {
 		const { responsiblesChannelId, teamNumber, isLastSubBoard } = data;
 		const message = `<!here>, The board of team ${teamNumber} is ready`;
-		this.chatHandler.postMessage(responsiblesChannelId, message);
-
-		await new Promise((r) => setTimeout(r, 1000));
+		await this.chatHandler.postMessage(responsiblesChannelId, message);
 
 		if (isLastSubBoard) {
 			const responsiblesMessage = `All sub-boards merged! Here's the complete board: ${this.frontendUrl}/boards/${data.mainBoardId}`;
-			this.chatHandler.postMessage(responsiblesChannelId, responsiblesMessage);
+			await this.chatHandler.postMessage(responsiblesChannelId, responsiblesMessage);
 		}
 
 		return data;
