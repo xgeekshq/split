1a620f3b5b56cfe9dbb089886431410fff465bf1
Nuno Caseiro <n.caseiro@kigroup.de>
2023-03-01 09:58:02 +0000

    fix: updatePhase arguments type

--- backend/src/modules/boards/repositories/board.repository.ts
+++ backend/src/modules/boards/repositories/board.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { BoardPhases } from 'src/libs/enum/board.phases';
 import { MongoGenericRepository } from 'src/libs/repositories/mongo/mongo-generic.repository';
 import Board, { BoardDocument } from 'src/modules/boards/entities/board.schema';
 import { BoardRepositoryInterface } from './board.repository.interface';
@@ -16,7 +17,8 @@ export class BoardRepository
 	getBoard(boardId: string): Promise<Board> {
 		return this.findOneById(boardId);
 	}
-	updatePhase(boardId, phase): Promise<Board> {
+
+	updatePhase(boardId: string, phase: BoardPhases): Promise<Board> {
 		return this.findOneByFieldAndUpdate(
 			{
 				_id: boardId
