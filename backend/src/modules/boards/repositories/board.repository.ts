07049806149a1fe2f0d85e8365907ea507b713a3
GoncaloCanteiro <g.canteiro@kigroup.de>
2023-03-02 11:05:52 +0000

    fix: add submitedAt date when submited

--- backend/src/modules/boards/repositories/board.repository.ts
+++ backend/src/modules/boards/repositories/board.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { BoardPhases } from 'src/libs/enum/board.phases';
 import { MongoGenericRepository } from 'src/libs/repositories/mongo/mongo-generic.repository';
 import Board, { BoardDocument } from 'src/modules/boards/entities/board.schema';
 import { BoardRepositoryInterface } from './board.repository.interface';
@@ -17,12 +18,17 @@ export class BoardRepository
 		return this.findOneById(boardId);
 	}
 	updatePhase(boardId, phase): Promise<Board> {
+		const submitedAt = phase !== BoardPhases.SUBMITED ? null : new Date();
+
 		return this.findOneByFieldAndUpdate(
 			{
 				_id: boardId
 			},
 			{
-				phase
+				$set: {
+					phase,
+					submitedAt
+				}
 			},
 			{ new: true },
 			{
