f842fdd7ad72444009a18a00954a787c4aa7f8fe
GoncaloCanteiro <gcanteiro15@gmail.com>
2023-03-09 11:36:10 +0000

    test: updateBoardPhase tests to boards.controller

--- backend/src/modules/boards/controller/boards.controller.spec.ts
+++ backend/src/modules/boards/controller/boards.controller.spec.ts
@@ -2,16 +2,19 @@ import { Test, TestingModule } from '@nestjs/testing';
 import BoardsController from 'src/modules/boards/controller/boards.controller';
 import * as Boards from 'src/modules/boards/interfaces/types';
 import SocketGateway from 'src/modules/socket/gateway/socket.gateway';
-import { createMock } from '@golevelup/ts-jest';
+import { DeepMocked, createMock } from '@golevelup/ts-jest';
 import { CreateBoardApplication } from '../applications/create.board.application';
 import { GetBoardApplication } from '../applications/get.board.application';
 import { UpdateBoardApplication } from '../applications/update.board.application';
 import { DeleteBoardApplication } from '../applications/delete.board.application';
 import { GetBoardGuard } from 'src/libs/guards/getBoardPermissions.guard';
 import { BoardUserGuard } from 'src/libs/guards/boardRoles.guard';
+import { BoardPhases } from 'src/libs/enum/board.phases';
 
 describe('BoardsController', () => {
 	let controller: BoardsController;
+	let updateBoardAppMock: DeepMocked<UpdateBoardApplication>;
+	const boardPhaseDto = { boardId: '6405f9a04633b1668f71c068', phase: BoardPhases.ADDCARDS };
 
 	beforeAll(async () => {
 		const module: TestingModule = await Test.createTestingModule({
@@ -46,9 +49,30 @@ describe('BoardsController', () => {
 			.compile();
 
 		controller = module.get<BoardsController>(BoardsController);
+		updateBoardAppMock = module.get(Boards.TYPES.applications.UpdateBoardApplication);
+	});
+
+	beforeEach(() => {
+		jest.clearAllMocks();
 	});
 
 	it('should be defined', () => {
 		expect(controller).toBeDefined();
+	});
+
+	describe('updateBoardPhase', () => {
+		it('should be defined', () => {
+			expect(controller.updateBoardPhase).toBeDefined();
+		});
+
+		it('should call updateBoardApp', async () => {
+			await controller.updateBoardPhase(boardPhaseDto);
+			expect(updateBoardAppMock.updatePhase).toHaveBeenCalledTimes(1);
+		});
+
+		it('should recieve boardPhaseDto in updateBoardApp', async () => {
+			await controller.updateBoardPhase(boardPhaseDto);
+			expect(updateBoardAppMock.updatePhase).toBeCalledWith(boardPhaseDto);
+		});
 	});
 });
