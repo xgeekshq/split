3d6a8b80d39ed07b3b401220c1540bce5a1a67b7
Guido <stereopt@gmail.com>
2023-03-10 14:31:06 +0000

    feat: coverage

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -18,6 +18,7 @@ const customJestConfig = {
   },
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  coveragePathIgnorePatterns: ['src/**/*.stories.tsx']
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
