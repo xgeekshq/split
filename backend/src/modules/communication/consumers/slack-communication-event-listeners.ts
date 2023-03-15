1999a6708634d0c805ed2e908b709854f37b95f4
GoncaloCanteiro <gcanteiro15@gmail.com>
2023-03-15 16:44:24 +0000

    fix: remove saveLog from communication listener

--- backend/src/modules/communication/consumers/slack-communication-event-listeners.ts
+++ backend/src/modules/communication/consumers/slack-communication-event-listeners.ts
@@ -2,7 +2,6 @@
 import { OnQueueCompleted, OnQueueError, OnQueueFailed, Process } from '@nestjs/bull';
 import { Logger } from '@nestjs/common';
 import { Job } from 'bull';
-import { createWriteStream } from 'fs';
 
 export class SlackCommunicationEventListeners<T, R> {
 	constructor(public logger: Logger) {}
@@ -13,9 +12,8 @@ export class SlackCommunicationEventListeners<T, R> {
 
 	// https://github.com/OptimalBits/bull/blob/develop/REFERENCE.md#events
 	@OnQueueCompleted()
-	async onCompleted(job: Job<T>, result: R[]) {
+	async onCompleted(job: Job<T>, _result: R[]) {
 		this.logger.verbose(`Completed Job id: "${job.id}"`);
-		this.saveLog(result);
 	}
 
 	@OnQueueFailed()
@@ -26,15 +24,5 @@ export class SlackCommunicationEventListeners<T, R> {
 	@OnQueueError()
 	onError(error: Error) {
 		this.logger.error(`Error on queue: "${JSON.stringify(error)}"`);
-	}
-
-	saveLog(data: any) {
-		try {
-			const stream = createWriteStream('slackLog.txt', { flags: 'a' });
-			stream.write(`${JSON.stringify(data)}\n`);
-			stream.end();
-		} catch (error) {
-			this.logger.error(error);
-		}
 	}
 }
