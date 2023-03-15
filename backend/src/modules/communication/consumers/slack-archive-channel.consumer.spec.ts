11a7e88124a77b92c535c6e219d02da2af8ce6b6
GoncaloCanteiro <gcanteiro15@gmail.com>
2023-03-15 11:02:13 +0000

    test: add Logger spys in slack-archive

--- backend/src/modules/communication/consumers/slack-archive-channel.consumer.spec.ts
+++ backend/src/modules/communication/consumers/slack-archive-channel.consumer.spec.ts
@@ -5,6 +5,7 @@ import { ArchiveChannelData } from '../dto/types';
 import { Job } from 'bull';
 import { SlackArchiveChannelConsumer } from './slack-archive-channel.consumer';
 import { ArchiveChannelApplicationInterface } from '../interfaces/archive-channel.application.interface';
+import { Logger } from '@nestjs/common';
 
 const archiveChannelDataMock = {
 	id: 1,
@@ -45,10 +46,25 @@ describe('SlackArchiveChannelConsumer', () => {
 	describe('communication', () => {
 		it('should call ArchiveChannelApplication.execute once with job.data', async () => {
 			await consumer.communication(archiveChannelDataMock as unknown as Job<ArchiveChannelData>);
+
 			expect(addUserIntoChannelAppMock.execute).toHaveBeenNthCalledWith(
 				1,
 				archiveChannelDataMock.data
 			);
+		});
+
+		it('should call Logger when type CHANNEL_ID', async () => {
+			const spyLogger = jest.spyOn(Logger.prototype, 'verbose');
+			//Type  'CHANNEL_ID'
+			await consumer.communication(archiveChannelDataMock as unknown as Job<ArchiveChannelData>);
+			expect(spyLogger).toBeCalledTimes(1);
+		});
+		it('should call Logger when type BOARD', async () => {
+			const spyLogger = jest.spyOn(Logger.prototype, 'verbose');
+			archiveChannelDataMock.data.type = 'BOARD';
+			//Type  'BOARD'
+			await consumer.communication(archiveChannelDataMock as unknown as Job<ArchiveChannelData>);
+			expect(spyLogger).toBeCalledTimes(1);
 		});
 	});
 });
