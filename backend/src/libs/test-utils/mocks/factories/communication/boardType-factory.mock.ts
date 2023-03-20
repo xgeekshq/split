1eda64b93173af68174aeefa595773fba075c02b
GoncaloCanteiro <gcanteiro15@gmail.com>
2023-03-20 16:09:23 +0000

    feat: boardTypeFactory

---
+++ backend/src/libs/test-utils/mocks/factories/communication/boardType-factory.mock.ts
@@ -0,0 +1,24 @@
+import faker from '@faker-js/faker';
+import { buildTestFactory } from '../generic-factory.mock';
+import { BoardType } from 'src/modules/communication/dto/types';
+import { UserRoleTypeFactory } from './UserRoleType-factory.mock';
+
+const mockBoardData = () => {
+	return {
+		id: faker.database.mongodbObjectId(),
+		title: faker.lorem.words(),
+		isSubBoard: faker.datatype.boolean(),
+		dividedBoards: [],
+		team: {
+			name: faker.lorem.words(),
+			users: UserRoleTypeFactory.createMany(3)
+		},
+		users: UserRoleTypeFactory.createMany(3),
+		slackChannelId: faker.lorem.word(),
+		boardNumber: Number(faker.random.numeric())
+	};
+};
+
+export const BoardTypeFactory = buildTestFactory<BoardType>(() => {
+	return mockBoardData();
+});
