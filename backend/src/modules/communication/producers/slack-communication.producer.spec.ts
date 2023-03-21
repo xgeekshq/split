a13200cec87b43b10f9aa320e7fb4199b2801984
GoncaloCanteiro <gcanteiro15@gmail.com>
2023-03-21 11:08:14 +0000

    test: slack-communication.producer   -test if queue is called with the right data   -test if logger is called with boardTypeID   -add BoardType-factory   -add UserRoleType-factory

--- backend/src/modules/communication/producers/slack-communication.producer.spec.ts
+++ backend/src/modules/communication/producers/slack-communication.producer.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { createMock } from '@golevelup/ts-jest';
+import { DeepMocked, createMock } from '@golevelup/ts-jest';
 import { Queue } from 'bull';
 import { getQueueToken } from '@nestjs/bull';
 import { SlackCommunicationProducer } from './slack-communication.producer';
+import { BoardTypeFactory } from 'src/libs/test-utils/mocks/factories/communication/boardType-factory.mock';
+import { Logger } from '@nestjs/common';
 
 describe('SlackArchiveChannelProducer', () => {
 	let producer: SlackCommunicationProducer;
-	//let queueMock: DeepMocked<Queue>;
+	let queueMock: DeepMocked<Queue>;
+
+	const spyLoggerVerbose = jest.spyOn(Logger.prototype, 'verbose').mockImplementation(jest.fn);
 
 	beforeAll(async () => {
 		const module: TestingModule = await Test.createTestingModule({
@@ -19,7 +23,7 @@ describe('SlackArchiveChannelProducer', () => {
 			]
 		}).compile();
 		producer = module.get<SlackCommunicationProducer>(SlackCommunicationProducer);
-		//queueMock = module.get(getQueueToken(SlackCommunicationProducer.name));
+		queueMock = module.get(getQueueToken(SlackCommunicationProducer.name));
 	});
 
 	beforeEach(() => {
@@ -28,5 +32,17 @@ describe('SlackArchiveChannelProducer', () => {
 
 	it('should be defined', () => {
 		expect(producer).toBeDefined();
+	});
+
+	it('should add boardType to queue', async () => {
+		const boardType = BoardTypeFactory.create();
+		await producer.add(boardType);
+		expect(queueMock.add).toHaveBeenNthCalledWith(1, boardType);
+	});
+
+	it('should call logger and containt board id', async () => {
+		const boardType = BoardTypeFactory.create();
+		await producer.add(boardType);
+		expect(spyLoggerVerbose).toHaveBeenNthCalledWith(1, expect.stringContaining(boardType.id));
 	});
 });
