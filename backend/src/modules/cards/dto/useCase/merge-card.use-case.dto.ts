a6c0135148824d5dd3d827a69aa8cb23a9c5f937
GoncaloCanteiro <gcanteiro15@gmail.com>
2023-03-27 12:02:27 +0100

    feat: add merge-card.use-case dto

---
+++ backend/src/modules/cards/dto/useCase/merge-card.use-case.dto.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export default class MergeCardUseCaseDto {
+	@ApiProperty()
+	@IsNotEmpty()
+	@IsString()
+	boardId: string;
+
+	@ApiProperty()
+	@IsNotEmpty()
+	@IsString()
+	draggedCardId: string;
+
+	@ApiProperty()
+	@IsNotEmpty()
+	IsString;
+	cardId: string;
+}
