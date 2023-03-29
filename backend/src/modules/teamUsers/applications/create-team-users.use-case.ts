b435d2226a13594f0b95eef40f19254ffd1f3054
Patrícia Dias <p.dias@kigroup.de>
2023-03-29 16:05:24 +0100

    feat: started refactor

--- backend/src/modules/teamUsers/applications/create-team-users.use-case.ts
+++ backend/src/modules/teamUsers/applications/create-team-users.use-case.ts
@@ -1,7 +1,9 @@
+import { GetTeamServiceInterface } from 'src/modules/teams/interfaces/services/get.team.service.interface';
 import TeamUserDto from 'src/modules/teamUsers/dto/team.user.dto';
 import { TeamUserRepositoryInterface } from '../interfaces/repositories/team-user.repository.interface';
 import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { TYPES } from '../interfaces/types';
+import * as Teams from 'src/modules/teams/interfaces/types';
 import { INSERT_FAILED } from 'src/libs/exceptions/messages';
 import TeamUser from 'src/modules/teamUsers/entities/team.user.schema';
 import { UseCase } from 'src/libs/interfaces/use-case.interface';
@@ -10,13 +12,17 @@ import { UseCase } from 'src/libs/interfaces/use-case.interface';
 export class CreateTeamUsersUseCase implements UseCase<TeamUserDto[], TeamUser[]> {
 	constructor(
 		@Inject(TYPES.repositories.TeamUserRepository)
-		private readonly teamUserRepository: TeamUserRepositoryInterface
+		private readonly teamUserRepository: TeamUserRepositoryInterface,
+		@Inject(Teams.TYPES.services.GetTeamService)
+		private getTeamService: GetTeamServiceInterface
 	) {}
 
 	async execute(teamUsers: TeamUserDto[]): Promise<TeamUser[]> {
 		const teamUsersSaved = await this.teamUserRepository.insertMany(teamUsers);
 
 		if (teamUsersSaved.length < 1) throw new BadRequestException(INSERT_FAILED);
+
+		const teamWithAddUser = await this.getTeamService.getTeam(teamUsers[0].team);
 
 		return teamUsersSaved;
 	}
