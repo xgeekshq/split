8fd0d42e7592461c581d3336cd8aa83a887346c4
Patrícia Dias <p.dias@kigroup.de>
2023-03-29 17:18:50 +0100

    fix: azure folder

--- backend/src/modules/azure/azure.module.ts
+++ backend/src/modules/azure/azure.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import AuthModule from '../../auth/auth.module';
-import { CommunicationModule } from '../../communication/communication.module';
-import { StorageModule } from '../../storage/storage.module';
-import UsersModule from '../../users/users.module';
+import AuthModule from '../auth/auth.module';
+import { CommunicationModule } from '../communication/communication.module';
+import { StorageModule } from '../storage/storage.module';
+import UsersModule from '../users/users.module';
 import { authAzureService, checkUserUseCase, registerOrLoginUseCase } from './azure.providers';
 import AzureController from './controller/azure.controller';
 
