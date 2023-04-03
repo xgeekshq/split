32233849fa3efa0db0b993712d0661a0b98b7199
GoncaloCanteiro <gcanteiro15@gmail.com>
2023-04-03 14:56:57 +0100

    fix: remove unused delete.card.service imports

--- backend/src/modules/cards/services/delete.card.service.ts
+++ backend/src/modules/cards/services/delete.card.service.ts
@@ -2,21 +2,14 @@ import { Inject, Injectable } from '@nestjs/common';
 import { DeleteVoteServiceInterface } from 'src/modules/votes/interfaces/services/delete.vote.service.interface';
 import * as Votes from 'src/modules/votes/interfaces/types';
 import { DeleteCardServiceInterface } from '../interfaces/services/delete.card.service.interface';
-import { GetCardServiceInterface } from '../interfaces/services/get.card.service.interface';
-import { TYPES } from '../interfaces/types';
 import Card from '../entities/card.schema';
-import { CardRepositoryInterface } from '../repository/card.repository.interface';
 import { getUserWithVotes } from '../utils/get-user-with-votes';
 
 @Injectable()
 export default class DeleteCardService implements DeleteCardServiceInterface {
 	constructor(
-		@Inject(TYPES.services.GetCardService)
-		private getCardService: GetCardServiceInterface,
 		@Inject(Votes.TYPES.services.DeleteVoteService)
-		private deleteVoteService: DeleteVoteServiceInterface,
-		@Inject(TYPES.repository.CardRepository)
-		private readonly cardRepository: CardRepositoryInterface
+		private deleteVoteService: DeleteVoteServiceInterface
 	) {}
 
 	async deleteCardVotesFromColumn(boardId: string, cardsArray: Card[]) {
