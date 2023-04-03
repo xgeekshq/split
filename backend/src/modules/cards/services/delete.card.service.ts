2da0e739d58904e09b8296ea3c96d19bcff2d106
GoncaloCanteiro <gcanteiro15@gmail.com>
2023-04-03 19:30:45 +0100

    fix: transaction and merge votes   -Merge card votes with item votes

--- backend/src/modules/cards/services/delete.card.service.ts
+++ backend/src/modules/cards/services/delete.card.service.ts
@@ -1,33 +1,85 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { DeleteVoteServiceInterface } from 'src/modules/votes/interfaces/services/delete.vote.service.interface';
-import * as Votes from 'src/modules/votes/interfaces/types';
 import { DeleteCardServiceInterface } from '../interfaces/services/delete.card.service.interface';
 import Card from '../entities/card.schema';
-import { getUserWithVotes } from '../utils/get-user-with-votes';
+import { getUserWithVotes, mergeTwoUsersWithVotes } from '../utils/get-user-with-votes';
+import * as BoardUsers from 'src/modules/boardUsers/interfaces/types';
+import { UpdateBoardUserServiceInterface } from 'src/modules/boardUsers/interfaces/services/update.board.user.service.interface';
+import { DELETE_VOTE_FAILED } from 'src/libs/exceptions/messages';
+import { DeleteFailedException } from 'src/libs/exceptions/deleteFailedBadRequestException';
 
 @Injectable()
 export default class DeleteCardService implements DeleteCardServiceInterface {
 	constructor(
-		@Inject(Votes.TYPES.services.DeleteVoteService)
-		private deleteVoteService: DeleteVoteServiceInterface
+		@Inject(BoardUsers.TYPES.services.UpdateBoardUserService)
+		private updateBoardUserService: UpdateBoardUserServiceInterface
 	) {}
 
 	async deleteCardVotesFromColumn(boardId: string, cardsArray: Card[]) {
-		cardsArray.forEach((cards) => {
-			cards.items.forEach(async (card) => {
-				const votesByUserOnCardItems = getUserWithVotes(card.votes);
+		await this.updateBoardUserService.startTransaction();
+		try {
+			await this.deleteVotesFromCards(boardId, cardsArray);
+			await this.updateBoardUserService.commitTransaction();
+		} catch (e) {
+			throw new DeleteFailedException();
+		} finally {
+			await this.updateBoardUserService.endSession();
+		}
+	}
 
-				votesByUserOnCardItems.forEach(async (votesCount, userId) => {
-					await this.deleteVoteService.decrementVoteUser(boardId, userId, -votesCount);
-				});
+	private async deleteVotesFromCards(boardId, cardsArray: Card[]) {
+		try {
+			const promises = cardsArray.map((card) => {
+				const usersWithVotes = this.mergeUsersVotes(card);
+
+				if (usersWithVotes.size > 0) {
+					return this.deletedVotesFromCard(boardId, usersWithVotes);
+				}
 			});
+			await Promise.all(promises);
+		} catch {
+			await this.updateBoardUserService.abortTransaction();
+			throw Error();
+		}
+	}
 
-			if (cards.votes.length > 0) {
-				const votesByUserOnCard = getUserWithVotes(cards.votes);
-				votesByUserOnCard.forEach(async (votesCount, userId) => {
-					await this.deleteVoteService.decrementVoteUser(boardId, userId, -votesCount);
-				});
+	private async deletedVotesFromCard(boardId: string, usersWithVotes) {
+		try {
+			const result = await this.updateBoardUserService.updateManyUserVotes(
+				boardId,
+				usersWithVotes,
+				true,
+				true
+			);
+
+			if (result.ok !== 1) {
+				throw new Error(DELETE_VOTE_FAILED);
 			}
-		});
+		} catch (e) {
+			throw new Error(e.message);
+		}
+	}
+
+	private mergeUsersVotes(card: Card): Map<string, number> {
+		let cardWithVotes: Map<string, number>;
+		let itemsWithVotes: Map<string, number>;
+		let usersWithVotes: Map<string, number>;
+
+		if (Object.keys(card.items).length === 1) {
+			usersWithVotes = getUserWithVotes(card.items[0].votes);
+		}
+
+		if (Object.keys(card.items).length > 1) {
+			card.items.forEach((item) => {
+				itemsWithVotes = getUserWithVotes(item.votes);
+				usersWithVotes = mergeTwoUsersWithVotes(usersWithVotes, itemsWithVotes);
+			});
+		}
+
+		if (card.votes?.length) {
+			cardWithVotes = getUserWithVotes(card.votes);
+			usersWithVotes = mergeTwoUsersWithVotes(usersWithVotes, cardWithVotes);
+		}
+
+		return usersWithVotes;
 	}
 }
--- backend/src/modules/cards/utils/get-user-with-votes.ts
+++ backend/src/modules/cards/utils/get-user-with-votes.ts
@@ -1,6 +1,7 @@
 import { ObjectId } from 'mongoose';
 import User from 'src/modules/users/entities/user.schema';
 
+//returns Map with the users and the amount of votes
 export const getUserWithVotes = (votes: string[] | User[] | ObjectId[]): Map<string, number> => {
 	const votesByUser = new Map<string, number>();
 	votes.forEach((userId) => {
@@ -13,4 +14,28 @@ export const getUserWithVotes = (votes: string[] | User[] | ObjectId[]): Map<str
 	});
 
 	return votesByUser;
+};
+
+//Merge 2 users With votes in 1 Map
+export const mergeTwoUsersWithVotes = (
+	userWithVotes1: Map<string, number>,
+	userWithVotes2: Map<string, number>
+) => {
+	if (!userWithVotes1 && userWithVotes2) {
+		return userWithVotes2;
+	}
+
+	if (userWithVotes1 && !userWithVotes2) {
+		return userWithVotes1;
+	}
+
+	for (const [key, value] of userWithVotes1) {
+		if (userWithVotes2.has(key)) {
+			userWithVotes2.set(key, userWithVotes2.get(key)! + value);
+		} else {
+			userWithVotes2.set(key, value);
+		}
+	}
+
+	return userWithVotes2;
 };
