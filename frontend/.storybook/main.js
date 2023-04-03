2ab506a393435cd7061ccd7a04eb4861eed375a8
Nuno Caseiro <90208434+nunocaseiro@users.noreply.github.com>
2023-04-03 11:32:47 +0100

    chore: update storybook (#1334)

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,37 +1,32 @@
 const path = require('path');
-
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    'storybook-addon-next',
-    '@tomfreudenberg/next-auth-mock/storybook'
-  ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@tomfreudenberg/next-auth-mock/storybook', '@storybook/addon-mdx-gfm'],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
   },
-  webpackFinal: async (config, { configType }) => {
+  webpackFinal: async (config, {
+    configType
+  }) => {
     config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules'];
     config.resolve.alias['@tomfreudenberg/next-auth-mock/storybook/preview-mock-auth-states'] = path.resolve(__dirname, 'previewMockAuthStates.js');
-
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname, '../src'),
+      '@': path.resolve(__dirname, '../src')
     };
-
-    if (process.env.NODE_ENV === 'production') {
-      config.output.publicPath = '/storybook/';
-    }
-
-    return config;
-  },
-  managerWebpack: async (config) => {
     if (process.env.NODE_ENV === 'production') {
       config.output.publicPath = '/storybook/';
     }
     return config;
   },
+  managerWebpack: async config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.output.publicPath = '/storybook/';
+    }
+    return config;
+  },
+  docs: {
+    autodocs: true
+  }
 };
