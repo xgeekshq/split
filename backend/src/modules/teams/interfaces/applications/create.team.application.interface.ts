54ce4776a684257144b3c0e52a3a0f5220db59c4
Patrícia Dias <p.dias@kigroup.de>
2023-04-04 16:48:22 +0100

    fix: merge conflict

--- backend/src/modules/teams/interfaces/applications/create.team.application.interface.ts
+++ backend/src/modules/teams/interfaces/applications/create.team.application.interface.ts
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-import { CreateTeamDto } from '../../dto/crate-team.dto';
-=======
-import { CreateTeamDto } from '../../dto/create-team.dto';
-import TeamUserDto from '../../../teamUsers/dto/team.user.dto';
-import TeamUser from 'src/modules/teamUsers/entities/team.user.schema';
->>>>>>> main
+import { CreateTeamDto } from 'src/modules/teams/dto/create-team.dto';
 import Team from '../../entities/team.schema';
 
 export interface CreateTeamApplicationInterface {
