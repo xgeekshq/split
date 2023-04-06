29e87377c85215bd0d1830741f6923622ed9a66f
Milton Boos Junior <junior_boos@live.com>
2023-04-06 10:06:00 +0100

    fix: merge conflicts

--- frontend/src/components/layouts/AccessLayout/styles.tsx
+++ frontend/src/components/layouts/AccessLayout/styles.tsx
@@ -2,6 +2,49 @@ import { styled } from '@/styles/stitches/stitches.config';
 
 import Flex from '@/components/Primitives/Layout/Flex/Flex';
 
+const CenteredContainer = styled('div', {
+  position: 'absolute',
+  top: '5%',
+  right: '$150',
+
+  maxWidth: '500px',
+  height: 'fit-content',
+
+  display: 'flex',
+  flexDirection: 'column',
+
+  backgroundColor: '#ffffff',
+  borderRadius: '$12',
+});
+
+const BodyContainer = styled(Flex, {
+  width: '100%',
+  margin: '$48',
+  gap: '$72',
+
+  '@md': {
+    margin: '$48 $72 $24',
+    width: '$362',
+  },
+});
+
+const FormContainer = styled(Flex, {
+  height: '100%',
+  justifyContent: 'space-between',
+});
+
+const ImageContainer = styled(Flex, {
+  flexGrow: 1,
+  py: '$24',
+  pr: '$24',
+  flexShrink: 0,
+  display: 'none',
+
+  '@md': {
+    display: 'flex',
+  },
+});
+
 const ImageBackground = styled(Flex, {
   height: '100%',
   width: '100%',
@@ -12,4 +55,17 @@ const ImageBackground = styled(Flex, {
   borderRadius: '$72 0 $72',
 });
 
-export { ImageBackground };
+const BannerContainer = styled(Flex, {
+  '& svg': {
+    maxWidth: '100%',
+  },
+});
+
+export {
+  BannerContainer,
+  CenteredContainer,
+  BodyContainer,
+  FormContainer,
+  ImageContainer,
+  ImageBackground,
+};
