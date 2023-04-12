821df4efff01a1dd20ea82c740b6918c24828f4b
JoaoSaIvador <j.salvador@kigroup.de>
2023-04-12 15:59:15 +0100

    test: add useUsers hook tests

---
+++ frontend/src/hooks/users/useUsers.spec.tsx
@@ -0,0 +1,58 @@
+import { renderHook, waitFor } from '@testing-library/react';
+import useUsers from '@/hooks/users/useUsers';
+import {
+  RenderHookWithProvidersOptions,
+  renderHookWithProviders,
+} from '@/utils/testing/renderHookWithProviders';
+import { UserFactory } from '@/utils/factories/user';
+import { getAllUsers } from '@/api/userService';
+import { User } from '@/types/user/user';
+import { toastState } from '@/store/toast/atom/toast.atom';
+import { ToastStateEnum } from '@/utils/enums/toast-types';
+
+const DUMMY_USERS = UserFactory.createMany(3);
+
+const mockGetAllUsers = getAllUsers as jest.Mock<Promise<User[]>>;
+jest.mock('@/api/userService');
+
+const render = (options?: Partial<RenderHookWithProvidersOptions>) =>
+  renderHook(() => useUsers(), { wrapper: renderHookWithProviders(options) });
+
+describe('api/useUsers', () => {
+  beforeEach(() => {
+    mockGetAllUsers.mockReturnValue(Promise.resolve(DUMMY_USERS));
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should fetch the users', async () => {
+    // Act
+    const { result } = render();
+
+    // Assert
+    await waitFor(() => expect(result.current.isSuccess).toBeTruthy());
+    expect(result.current.data).toBe(DUMMY_USERS);
+  });
+
+  it('should get error', async () => {
+    // Arrange
+    mockGetAllUsers.mockReturnValueOnce(Promise.reject(new Error('failed to fetch users')));
+    const recoilHandler = jest.fn();
+
+    // Act
+    const { result } = render({ recoilOptions: { recoilState: toastState, recoilHandler } });
+
+    // Assert
+    await waitFor(() => {
+      expect(result.current.isError).toBeTruthy();
+      expect(result.current.data).not.toBeDefined();
+      expect(recoilHandler).toHaveBeenCalledWith({
+        open: true,
+        content: 'Error getting the users',
+        type: ToastStateEnum.ERROR,
+      });
+    });
+  });
+});
---
+++ frontend/src/utils/testing/renderHookWithProviders.tsx
@@ -0,0 +1,54 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RenderOptions } from '@testing-library/react';
+import { ReactNode, useEffect } from 'react';
+import { RecoilRoot, RecoilState, useRecoilValue } from 'recoil';
+
+type RecoilOptions = {
+  recoilHandler: (value: any) => void;
+  recoilState: RecoilState<any>;
+};
+
+export type RenderHookWithProvidersOptions = Omit<RenderOptions, 'queries'> & {
+  queryClient?: QueryClient;
+  recoilOptions?: RecoilOptions;
+};
+
+export type WrapperProps = {
+  children: ReactNode;
+  queryClient: QueryClient;
+  recoilHandler?: (value: any) => void;
+  recoilState?: RecoilState<any>;
+};
+
+const RecoilObserver = ({ recoilState, recoilHandler }: RecoilOptions) => {
+  const value = useRecoilValue(recoilState);
+  useEffect(() => recoilHandler(value), [recoilHandler, value]);
+  return null;
+};
+
+export function createWrapper({ children, queryClient, recoilHandler, recoilState }: WrapperProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        {recoilHandler && recoilState && (
+          <RecoilObserver recoilState={recoilState} recoilHandler={recoilHandler} />
+        )}
+        {children}
+      </RecoilRoot>
+    </QueryClientProvider>
+  );
+}
+
+export function renderHookWithProviders(options?: Partial<RenderHookWithProvidersOptions>) {
+  const defaultQueryOptions = {
+    defaultOptions: {
+      queries: { retry: false },
+    },
+  };
+  const queryClient = options?.queryClient ?? new QueryClient(defaultQueryOptions);
+  const recoilHandler = options?.recoilOptions?.recoilHandler;
+  const recoilState = options?.recoilOptions?.recoilState;
+
+  return ({ children }: { children: ReactNode }) =>
+    createWrapper({ children, queryClient, recoilHandler, recoilState });
+}
