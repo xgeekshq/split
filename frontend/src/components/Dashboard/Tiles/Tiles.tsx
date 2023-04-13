40d24ac7fac3f8835c2f55ecf0367abd3dc02140
Milton Boos Junior <junior_boos@live.com>
2023-04-13 15:02:16 +0100

    refactor: replace TileContainer with Flex

--- frontend/src/components/Dashboard/Tiles/Tiles.tsx
+++ frontend/src/components/Dashboard/Tiles/Tiles.tsx
@@ -1,18 +1,18 @@
 import { HeaderInfo } from '@/types/dashboard/header.info';
 import { BOARDS_ROUTE, TEAMS_ROUTE, USERS_ROUTE } from '@/utils/routes';
-import { TileContainer } from '@/components/Dashboard/Tiles/styles';
 import Tile from '@/components/Dashboard/Tiles/Tile/Tile';
+import Flex from '@/components/Primitives/Layout/Flex/Flex';
 
 export type TilesProps = {
   data: HeaderInfo;
 };
 
 const Tiles = ({ data }: TilesProps) => (
-  <TileContainer as="section">
+  <Flex as="section" gap="26" wrap="wrap" css={{ '> *': { flex: 1 } }}>
     <Tile link={BOARDS_ROUTE} title="Your boards" count={data.boardsCount} color="purple" />
     <Tile link={TEAMS_ROUTE} title="Your teams" count={data.teamsCount} color="blue" />
     <Tile link={USERS_ROUTE} title="Active Members" count={data.usersCount} color="yellow" />
-  </TileContainer>
+  </Flex>
 );
 
 export default Tiles;
