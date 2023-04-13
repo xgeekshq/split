cbb93a75b83849ce1eaa74a43f0bee59038c912e
GoncaloCanteiro <gcanteiro15@gmail.com>
2023-04-13 15:14:36 +0100

    feat: add UncontrolledInput and story

---
+++ frontend/src/components/Primitives/Inputs/UncontrolledInput/UncontrolledInput.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+
+import Icon from '@/components/Primitives/Icons/Icon/Icon';
+import Flex from '@/components/Primitives/Layout/Flex/Flex';
+
+import {
+  IconWrapper,
+  PlaceholderText,
+  StyledInput,
+  StyledInputWrapper,
+} from '@/components/Primitives/Inputs/Input/styles';
+
+export type SearchInputProps = {
+  placeholder: string;
+  iconName?: string;
+  cursorType?: string;
+  IconPositionRight?: boolean;
+  disabled?: boolean;
+  currentValue?: string;
+  handleChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleClear?: () => void;
+};
+
+const UncontrolledInput = ({
+  placeholder,
+  IconPositionRight = false,
+  cursorType,
+  iconName = '',
+  disabled = false,
+  currentValue,
+  handleChange,
+  handleClear,
+}: SearchInputProps) => (
+  <Flex
+    css={{ position: 'relative', width: '100%', mb: '$16', height: 'auto' }}
+    direction="column"
+    data-testid="searchInput"
+  >
+    <StyledInputWrapper gap="8" disabled={disabled}>
+      <Flex style={{ order: IconPositionRight ? 2 : 1 }}>
+        <IconWrapper>
+          <Icon
+            name={iconName}
+            css={{
+              color: '$primary300',
+            }}
+          />
+        </IconWrapper>
+      </Flex>
+
+      <Flex css={{ flexGrow: '1' }} style={{ order: IconPositionRight ? 1 : 2 }}>
+        <StyledInput
+          id="searchInput"
+          disabled={disabled}
+          placeholder=" "
+          value={currentValue}
+          onChange={handleChange}
+          css={{ pt: '$28', pb: '$8' }}
+          style={{ cursor: cursorType || '' }}
+        />
+        <PlaceholderText as="label" htmlFor="searchInput">
+          {placeholder}
+        </PlaceholderText>
+      </Flex>
+      {handleClear && (
+        <IconWrapper
+          justify="center"
+          data-type="clear"
+          css={{
+            width: '$24',
+            height: '$24',
+            borderRadius: '100%',
+            order: 3,
+          }}
+          onClick={handleClear}
+          data-testid="searchInputClear"
+        >
+          <Icon
+            name="close"
+            size={16}
+            css={{
+              color: '$white',
+              backgroundColor: '$primary100',
+              borderRadius: '100%',
+              padding: '$2',
+            }}
+          />
+        </IconWrapper>
+      )}
+    </StyledInputWrapper>
+  </Flex>
+);
+
+export default UncontrolledInput;
