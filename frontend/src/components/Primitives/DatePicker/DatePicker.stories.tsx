698cc44b9a651cfdfb3b6c2da4a7179897b7cb2e
GoncaloCanteiro <gcanteiro15@gmail.com>
2023-04-13 17:56:18 +0100

    feat: add datePicker story

---
+++ frontend/src/components/Primitives/DatePicker/DatePicker.stories.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { ComponentStory } from '@storybook/react';
+import dedent from 'ts-dedent';
+import DatePicker from '@/components/Primitives/DatePicker/DatePicker';
+
+export default {
+  title: 'Primitives/DatePicker/DatePicker',
+  component: DatePicker,
+  parameters: {
+    docs: {
+      description: {
+        component: dedent`
+        
+        The DatePicker component allows users to easily choose a date and display it.
+
+        **File Path:**
+        \`@/components/Primitives/DatePicker/DatePicker\``,
+      },
+    },
+  },
+  argTypes: {
+    setDate: {
+      control: false,
+      description: 'Event handler called when the date state of the calendar changes.',
+      table: {
+        type: { summary: '(date: Date) => void' },
+      },
+    },
+    currentDate: {
+      control: false,
+      description: 'The current date of the calendar',
+      table: {
+        type: { summary: 'Date' },
+      },
+    },
+  },
+};
+
+const Template: ComponentStory<typeof DatePicker> = () => {
+  const [currentDate, setDate] = useState<Date>();
+
+  return (
+    <>
+      <DatePicker currentDate={currentDate} setDate={setDate} />
+    </>
+  );
+};
+
+export const Default = Template.bind({});
+Default.storyName = 'Basic Usage';
