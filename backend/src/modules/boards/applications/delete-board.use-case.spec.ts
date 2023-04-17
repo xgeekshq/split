2ddf03aabeca63c132f444e8aa43e6c941458b60
Guido Pereira <stereopt@gmail.com>
2023-04-17 12:33:13 +0100

    fix: accessing deleted board (#1402)

--- backend/src/modules/boards/applications/delete-board.use-case.spec.ts
+++ backend/src/modules/boards/applications/delete-board.use-case.spec.ts
@@ -7,6 +7,7 @@ import { deleteBoardUseCase } from '../boards.providers';
 import { DeleteBoardServiceInterface } from '../interfaces/services/delete.board.service.interface';
 import { BoardRepositoryInterface } from '../repositories/board.repository.interface';
 import { NotFoundException } from '@nestjs/common';
+import DeleteBoardUseCaseDto from 'src/modules/boards/dto/useCase/delete-board.use-case';
 
 const board = BoardFactory.create({
 	dividedBoards: BoardFactory.createMany(2),
@@ -14,7 +15,7 @@ const board = BoardFactory.create({
 });
 
 describe('DeleteBoardUseCase', () => {
-	let useCase: UseCase<string, boolean>;
+	let useCase: UseCase<DeleteBoardUseCaseDto, boolean>;
 	let deleteBoardServiceMock: DeepMocked<DeleteBoardServiceInterface>;
 	let boardRepositoryMock: DeepMocked<BoardRepositoryInterface>;
 
@@ -40,6 +41,7 @@ describe('DeleteBoardUseCase', () => {
 
 	beforeEach(() => {
 		jest.clearAllMocks();
+		boardRepositoryMock.getBoard.mockResolvedValue(board);
 	});
 
 	it('should be defined', () => {
@@ -52,13 +54,43 @@ describe('DeleteBoardUseCase', () => {
 		it('should return true if execute succeeds', async () => {
 			deleteBoardServiceMock.deleteBoardBoardUsersAndSchedules.mockResolvedValue(true);
 
-			await expect(useCase.execute('boardId')).resolves.toBe(true);
+			await expect(
+				useCase.execute({ boardId: 'boardId', completionHandler: jest.fn() })
+			).resolves.toBe(true);
 		});
 
 		it('should throw notFoundException when board not found ', async () => {
 			boardRepositoryMock.getBoard.mockResolvedValue(null);
 
-			await expect(useCase.execute('boardId')).rejects.toThrow(NotFoundException);
+			await expect(
+				useCase.execute({ boardId: 'boardId', completionHandler: jest.fn() })
+			).rejects.toThrow(NotFoundException);
+		});
+
+		it('should call completionHandler when execute board is deleted', async () => {
+			deleteBoardServiceMock.deleteBoardBoardUsersAndSchedules.mockResolvedValue(true);
+			const completionHandlerMock = jest.fn();
+
+			await expect(
+				useCase.execute({
+					boardId: 'boardId',
+					completionHandler: completionHandlerMock
+				})
+			).resolves.toBe(true);
+			expect(completionHandlerMock).toBeCalled();
+		});
+
+		it("shouldn't call completionHandler when execute board isn't deleted", async () => {
+			deleteBoardServiceMock.deleteBoardBoardUsersAndSchedules.mockResolvedValue(false);
+			const completionHandlerMock = jest.fn();
+
+			await expect(
+				useCase.execute({
+					boardId: 'boardId',
+					completionHandler: completionHandlerMock
+				})
+			).resolves.toBe(false);
+			expect(completionHandlerMock).not.toBeCalled();
 		});
 	});
 });
