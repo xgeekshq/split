5a7602e897aed2ca8c57a135266c444f1c1ca422
JoaoSaIvador <j.salvador@kigroup.de>
2023-04-18 17:00:29 +0100

    test: update createTeam tests

--- frontend/src/components/Teams/CreateTeam/CreateTeam.spec.tsx
+++ frontend/src/components/Teams/CreateTeam/CreateTeam.spec.tsx
@@ -1,11 +1,31 @@
+import { UseMutationResult } from '@tanstack/react-query';
 import { fireEvent, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import CreateTeam from '@/components/Teams/CreateTeam/CreateTeam';
 import { renderWithProviders } from '@/utils/testing/renderWithProviders';
+import useCreateTeam from '@hooks/teams/useCreateTeam';
+import { ROUTES } from '@utils/routes';
+import { libraryMocks } from '@utils/testing/mocks';
 
-const render = () => renderWithProviders(<CreateTeam />);
+const { mockRouter } = libraryMocks.mockNextRouter({ pathname: ROUTES.NewTeam });
+
+const render = () => renderWithProviders(<CreateTeam />, { routerOptions: mockRouter });
+
+const mockUseCreateTeam = useCreateTeam as jest.Mock<Partial<UseMutationResult>>;
+jest.mock('@/hooks/teams/useCreateTeam');
 
 describe('Teams/CreateTeam', () => {
+  beforeEach(() => {
+    mockUseCreateTeam.mockReturnValue({
+      mutate: jest.fn(),
+    } as Partial<UseMutationResult>);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should render correctly', () => {
     // Act
     const { getByTestId, queryByLabelText } = render();
@@ -22,12 +42,48 @@ describe('Teams/CreateTeam', () => {
   it('should open a UserListDialog', async () => {
     // Act
     const { getByTestId } = render();
+    fireEvent.click(getByTestId('addRemoveMembersTrigger'));
 
     // Assert
-    fireEvent.click(getByTestId('addRemoveMembersTrigger'));
 
     await waitFor(() => {
       expect(getByTestId('userListDialog')).toBeInTheDocument();
+    });
+  });
+
+  it('should handle create team', async () => {
+    // Arrange
+    const createTeamMutation = jest.fn();
+
+    mockUseCreateTeam.mockReturnValue({
+      mutate: createTeamMutation,
+      status: 'success',
+    } as Partial<UseMutationResult>);
+
+    // Act
+    const { getByText, getByLabelText } = render();
+
+    await userEvent.type(getByLabelText('Team name'), 'MyTeam');
+
+    fireEvent.click(getByText('Create team'));
+
+    // Assert
+
+    await waitFor(() => {
+      expect(createTeamMutation).toBeCalledWith({ name: 'MyTeam', users: [] });
+      expect(mockRouter.push).toHaveBeenCalledWith(ROUTES.Teams);
+    });
+  });
+
+  it('should redirect back', async () => {
+    // Act
+    const { getByTestId } = render();
+    fireEvent.click(getByTestId('backButton'));
+
+    // Assert
+
+    await waitFor(() => {
+      expect(mockRouter.back).toHaveBeenCalled();
     });
   });
 });
