90226652dbcb9b0d6206f23ae3c613aa512a81c9
JoaoSaIvador <j.salvador@kigroup.de>
2023-04-20 15:28:29 +0100

    fix: regular board name max chars

--- frontend/src/schema/schemaCreateRegularBoard.ts
+++ frontend/src/schema/schemaCreateRegularBoard.ts
@@ -1,8 +1,8 @@
 import Joi from 'joi';
 
 const SchemaCreateRegularBoard = Joi.object({
-  text: Joi.string().allow('').optional().max(30).messages({
-    'string.max': 'Maximum of 30 characters',
+  text: Joi.string().allow('').optional().max(45).messages({
+    'string.max': 'Maximum of 45 characters',
   }),
   maxVotes: Joi.number().integer().min(1).optional().messages({
     'number.min': 'Please insert a number greater than zero.',
