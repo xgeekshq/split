de151608d8ee77b8f3c1bf676a5f820641758b1c
Cátia Antunes <c.antunes@kigroup.de>
2023-04-20 18:18:22 +0100

    refactor: remove commented lines

--- backend/src/modules/cards/constants.ts
+++ backend/src/modules/cards/constants.ts
@@ -1,23 +1,3 @@
-export const TYPES = {
-	services: {
-		//GetCardService: 'GetCardService',
-		//DeleteFromCardGroupUseCase: 'DeleteFromCardGroupUseCase'
-	},
-	applications: {
-		//CreateCardUseCase: 'CreateCardUseCase',
-		//UnmergeCardUseCase: 'UnmergeCardUseCase',
-		//MergeCardUseCase: 'MergeCardUseCase',
-		//UpdateCardPositionUseCase: 'UpdateCardPositionUseCase',
-		//UpdateCardTextUseCase: 'UpdateCardTextUseCase',
-		//UpdateCardGroupTextUseCase: 'UpdateCardGroupTextUseCase',
-		//DeleteCardUseCase: 'DeleteCardUseCase',
-		//DeleteFromCardGroupUseCase: 'DeleteFromCardGroupUseCase'
-	},
-	repository: {
-		//CardRepository: 'CardRepository'
-	}
-};
-
 /* SERVICE */
 
 export const GET_CARD_SERVICE = 'GetCardService';
