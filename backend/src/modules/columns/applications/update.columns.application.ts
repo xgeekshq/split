cbc33c7d151eab7eb440458c8d6b990e72e0237a
Cátia Antunes <c.antunes@kigroup.de>
2023-04-24 10:37:55 +0100

    refactor: change TYPES for CONSTANTS (column)

--- backend/src/modules/columns/applications/update.columns.application.ts
+++ backend/src/modules/columns/applications/update.columns.application.ts
@@ -3,12 +3,12 @@ import { ColumnDeleteCardsDto } from '../dto/colum.deleteCards.dto';
 import { UpdateColumnDto } from '../dto/update-column.dto';
 import { UpdateColumnApplicationInterface } from '../interfaces/applications/update.comment.application.interface';
 import { UpdateColumnServiceInterface } from '../interfaces/services/update.column.service.interface';
-import { TYPES } from '../interfaces/types';
+import { UPDATE_COLUMN_SERVICE } from 'src/modules/columns/constants';
 
 @Injectable()
 export class UpdateColumnApplication implements UpdateColumnApplicationInterface {
 	constructor(
-		@Inject(TYPES.services.UpdateColumnService)
+		@Inject(UPDATE_COLUMN_SERVICE)
 		private updateColumnService: UpdateColumnServiceInterface
 	) {}
 
