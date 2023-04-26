ecd2b93c01fc3534a1aed54ecd8b5f6cd16858d7
Milton Boos Junior <junior_boos@live.com>
2023-04-26 11:44:56 +0100

    feat: add board card responsiveness (#1425)

--- frontend/src/components/CardBoard/CardBody/LeftArrow.tsx
+++ frontend/src/components/CardBoard/CardBody/LeftArrow.tsx
@@ -3,49 +3,59 @@ import React from 'react';
 import Icon from '@/components/Primitives/Icons/Icon/Icon';
 import Flex from '@/components/Primitives/Layout/Flex/Flex';
 
-type LeftArrowProps = {
+export type LeftArrowProps = {
+  /*  isLast is used to add a vertical bar at the end, 
+      so that it connects to the next element if it's not the last one */
+  isLast: boolean;
   isDashboard: boolean;
   index: number | undefined;
 };
 
-const LeftArrow = ({ isDashboard, index }: LeftArrowProps) => (
-  <Flex>
-    <Flex css={{ position: 'relative', size: 0 }}>
-      {(isDashboard || index === 0) && (
-        <Flex css={{ mt: '$11', ml: '$8', mr: '$7' }}>
-          <Icon
-            name="arrow_long"
-            css={{
-              width: '$18',
-              height: '33px',
-              color: '$primary100',
-            }}
-          />
-          {/* <LateralUpArrow /> */}
-        </Flex>
-      )}
-      {!isDashboard && index !== 0 && (
-        <Flex
+const LeftArrow = ({ isDashboard, isLast, index }: LeftArrowProps) => (
+  <Flex aria-hidden css={{ position: 'relative', width: 'fit-content' }}>
+    {(isDashboard || index === 0) && (
+      <Flex
+        css={{
+          position: 'absolute',
+          top: '$11',
+          left: '$8',
+        }}
+      >
+        <Icon
+          name="arrow_long"
           css={{
-            mt: '$2',
-            ml: '13px',
-            mr: '$4',
-            position: 'relative',
-            bottom: '42px',
+            width: '$18',
+            height: '33px',
+            color: '$primary100',
           }}
-        >
-          <Icon
-            name="line_long"
-            css={{
-              width: '13px',
-              height: '82px',
-              color: '$primary100',
-            }}
-          />
-          {/* <StartArrow /> */}
-        </Flex>
-      )}
-    </Flex>
+        />
+      </Flex>
+    )}
+    {!isDashboard && index !== 0 && (
+      <Flex
+        css={{
+          ml: '13px',
+          position: 'absolute',
+          bottom: '50%',
+          borderLeft: '2px solid $primary100',
+          borderBottom: '2px solid $primary100',
+          height: '70%',
+          minWidth: '$12',
+          borderBottomLeftRadius: '$5',
+        }}
+      />
+    )}
+    {!isLast && (
+      <Flex
+        css={{
+          ml: '13px',
+          position: 'absolute',
+          top: '20%',
+          borderLeft: '2px solid $primary100',
+          height: '100%',
+        }}
+      />
+    )}
   </Flex>
 );
 
