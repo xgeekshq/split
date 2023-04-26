4bc0908e1cf2f9a1d5284ba51f81f15dd7032c6b
Gonçalo Abreu Canteiro <58892791+GoncaloCanteiro@users.noreply.github.com>
2023-04-26 17:12:56 +0100

    feat: add scheduling form to board settings (#1430)

---
+++ frontend/src/components/Board/Settings/partials/Scheduling/Reminder/Reminder.tsx
@@ -0,0 +1,105 @@
+import { Dispatch, SetStateAction, useState } from 'react';
+
+import { UpdateScheduleType } from '@/types/board/board';
+import SelectDateUnit from '@components/Board/Settings/partials/Scheduling/SelectDateUnit';
+import Checkbox from '@components/Primitives/Inputs/Checkboxes/Checkbox/Checkbox';
+import Flex from '@components/Primitives/Layout/Flex/Flex';
+
+export type SchedulingProps = {
+  schedulingData: UpdateScheduleType;
+  setSchedulingData: Dispatch<SetStateAction<UpdateScheduleType>>;
+};
+
+const ReminderSchedule = ({ schedulingData, setSchedulingData }: SchedulingProps) => {
+  const [isReminderActive, setReminderState] = useState<boolean>(false);
+
+  const handleReminderActiveChange = (isActive: boolean) => {
+    setReminderState(isActive);
+    if (!isActive) {
+      setSchedulingData((prev: UpdateScheduleType) => ({
+        ...prev,
+        reminderTimeRange: undefined,
+        reminderTimeUnit: undefined,
+        reminderViaEmail: false,
+        reminderPrefillingCards: false,
+        reminderViaSlack: false,
+      }));
+    }
+  };
+
+  const handleReminderTimeRangeChange = (timeRange: string) => {
+    setSchedulingData((prev: UpdateScheduleType) => ({
+      ...prev,
+      reminderTimeRange: timeRange,
+    }));
+  };
+
+  const handleReminderTimeUnitChange = (timeUnit: string) => {
+    setSchedulingData((prev: UpdateScheduleType) => ({
+      ...prev,
+      reminderTimeUnit: timeUnit,
+    }));
+  };
+
+  const handleSlackChange = (viaSlackActive: boolean) => {
+    setSchedulingData((prev: UpdateScheduleType) => ({
+      ...prev,
+      reminderViaSlack: viaSlackActive,
+    }));
+  };
+  const handleEmailChange = (viaEmailActive: boolean) => {
+    setSchedulingData((prev: UpdateScheduleType) => ({
+      ...prev,
+      reminderViaEmail: viaEmailActive,
+    }));
+  };
+
+  const handlePrefillingCardChange = (prefillingCards: boolean) => {
+    setSchedulingData((prev: UpdateScheduleType) => ({
+      ...prev,
+      reminderPrefillingCards: prefillingCards,
+    }));
+  };
+  return (
+    <Flex direction="column" gap={16}>
+      <SelectDateUnit
+        description="Send reminder minutes/days/weeks before"
+        isChecked={isReminderActive}
+        setCheckboxState={handleReminderActiveChange}
+        setTimeRange={handleReminderTimeRangeChange}
+        setUnitTime={handleReminderTimeUnitChange}
+        timeRange={schedulingData.reminderTimeRange}
+        timeUnit={schedulingData.reminderTimeUnit}
+        title="Reminder"
+      />
+      <Flex css={{ ml: '$35' }} direction="column">
+        <Checkbox
+          checked={schedulingData.reminderViaSlack}
+          disabled={!isReminderActive}
+          handleChange={handleSlackChange}
+          id="repeatCheckbox"
+          label="Via Slack"
+          size="md"
+        />
+        <Checkbox
+          checked={schedulingData.reminderViaEmail}
+          disabled={!isReminderActive}
+          handleChange={handleEmailChange}
+          id="repeatCheckbox"
+          label="Via Email"
+          size="md"
+        />
+        <Checkbox
+          checked={schedulingData.reminderPrefillingCards}
+          disabled={!isReminderActive}
+          handleChange={handlePrefillingCardChange}
+          id="repeatCheckbox"
+          label="Remind prefilling the cards"
+          size="md"
+        />
+      </Flex>
+    </Flex>
+  );
+};
+
+export default ReminderSchedule;
---
+++ frontend/src/constants/schedulingOptions/weeks.ts
@@ -0,0 +1,32 @@
+export const WEEK_DAYS_FULL_NAME = [
+  {
+    label: 'Monday',
+    value: 'monday',
+  },
+  {
+    label: 'Tuesday',
+    value: 'tuesday',
+  },
+  {
+    label: 'Wednesday',
+    value: 'wednesday',
+  },
+  {
+    label: 'Thursday',
+    value: 'thursday',
+  },
+  {
+    label: 'Friday',
+    value: 'friday',
+  },
+  {
+    label: 'Saturday',
+    value: 'saturday',
+  },
+  {
+    label: 'Sunday',
+    value: 'sunday',
+  },
+];
+
+export const WEEK_DAYS_ABREVIATED = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
