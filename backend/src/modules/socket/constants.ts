68c118a32cf570bd87dca0dc29fd27dc548c6977
Cátia Antunes <c.antunes@kigroup.de>
2023-04-27 11:32:01 +0100

    refactor: change types for constants (socket) (#1437)

---
+++ backend/src/modules/socket/constants.ts
@@ -0,0 +1,15 @@
+/* SUBSCRIBERS */
+export const AFTER_SERVER_UPDATED_TIME_DURATION_SUBSCRIBER =
+	'AfterServerUpdatedTimerDurationSubscriber';
+
+export const AFTER_SERVER_PAUSED_TIMER_SUBSCRIBER = 'AfterServerPausedTimerSubscriber';
+
+export const AFTER_SERVER_STARTED_TIMER_SUBSCRIBER = 'AfterServerStaredTimerSubscriber';
+
+export const AFTER_SERVER_STOPPED_TIMER_SUBSCRIBER = 'AfterServerStoppedTimerSubscriber';
+
+export const AFTER_SERVER_UPDATED_TIME_LEFT_SUBSCRIBER = 'AfterServerUpdatedTimeLeftSubscriber';
+
+export const AFTER_SERVER_SENT_TIMER_STATE_SUBSCRIBER = 'AfterServerSentTimerStateSubscriber';
+
+export const AFTER_SERVER_UPDATED_PHASE_SUBSCRIBER = 'AfterServerUpdatedPhaseSubscriber';
