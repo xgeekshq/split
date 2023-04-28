0caf48ceef6487bbceab1e82d0834522b920bbf6
Cátia Antunes <c.antunes@kigroup.de>
2023-04-28 10:04:07 +0100

    fix: fix tests and refactor them to use go/level up lib

--- backend/src/modules/auth/services/validate-user.auth.service.spec.ts
+++ backend/src/modules/auth/services/validate-user.auth.service.spec.ts
@@ -1,112 +1,57 @@
-import { getTeamUserService, teamUserRepository } from '../../teamUsers/teamusers.providers';
-import { ConfigService } from '@nestjs/config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import { JwtService } from '@nestjs/jwt';
-import { getModelToken } from '@nestjs/mongoose';
 import { Test } from '@nestjs/testing';
 import * as bcrypt from 'bcrypt';
-import configService from 'src/libs/test-utils/mocks/configService.mock';
-import jwtService from 'src/libs/test-utils/mocks/jwtService.mock';
-import mockedUser from 'src/libs/test-utils/mocks/user.mock';
 import ValidateUserAuthService from 'src/modules/auth/services/validate-user.auth.service';
-import { boardRepository, getBoardService } from 'src/modules/boards/boards.providers';
-import {
-	boardUserRepository,
-	createBoardUserService,
-	getBoardUserService
-} from 'src/modules/boardUsers/boardusers.providers';
-import SocketGateway from 'src/modules/socket/gateway/socket.gateway';
-import { getTeamService, teamRepository } from 'src/modules/teams/providers';
+import { RESET_PASSWORD_REPOSITORY } from 'src/modules/auth/constants';
+import { DeepMocked, createMock } from '@golevelup/ts-jest';
+import { ResetPasswordRepositoryInterface } from 'src/modules/auth/repository/reset-password.repository.interface';
 import { TYPES } from 'src/modules/users/interfaces/types';
-import GetUserService from 'src/modules/users/services/get.user.service';
-import {
-	getUserService,
-	updateUserService,
-	userRepository
-} from 'src/modules/users/users.providers';
-import { getTokenAuthService, resetPasswordRepository } from '../auth.providers';
+import { GetUserServiceInterface } from 'src/modules/users/interfaces/services/get.user.service.interface';
+import { UserFactory } from 'src/libs/test-utils/mocks/factories/user-factory';
 
 jest.mock('bcrypt');
 jest.mock('src/modules/schedules/services/create.schedules.service.ts');
 jest.mock('src/modules/schedules/services/delete.schedules.service.ts');
 
+const user = UserFactory.create();
+
 describe('The AuthenticationService', () => {
 	let authenticationService: ValidateUserAuthService;
-	let gUserService: GetUserService;
+	let getUserServiceMock: DeepMocked<GetUserServiceInterface>;
 	let bcryptCompare: jest.Mock;
-	let findUser: jest.Mock;
-	beforeEach(async () => {
-		findUser = jest.fn().mockImplementation(() => ({
-			exec: jest.fn().mockReturnValue(mockedUser)
-		}));
-		const usersRepository = {
-			findOne: findUser
-		};
 
-		bcryptCompare = jest.fn().mockReturnValue(true);
-		(bcrypt.compare as jest.Mock) = bcryptCompare;
-
+	beforeAll(async () => {
 		const module = await Test.createTestingModule({
 			imports: [EventEmitterModule.forRoot()],
 			providers: [
 				ValidateUserAuthService,
-				SocketGateway,
-				getUserService,
-				getTeamService,
-				userRepository,
-				teamRepository,
-				getBoardService,
-				createBoardUserService,
-				getTokenAuthService,
-				boardUserRepository,
-				boardRepository,
-				updateUserService,
-				getBoardUserService,
-				getTeamUserService,
-				teamUserRepository,
-				resetPasswordRepository,
 				{
-					provide: ConfigService,
-					useValue: configService
+					provide: RESET_PASSWORD_REPOSITORY,
+					useValue: createMock<ResetPasswordRepositoryInterface>()
 				},
 				{
-					provide: JwtService,
-					useValue: jwtService
-				},
-				{
-					provide: getModelToken('Board'),
-					useValue: {}
-				},
-				{
-					provide: getModelToken('BoardUser'),
-					useValue: {}
-				},
-				{
-					provide: getModelToken('User'),
-					useValue: usersRepository
-				},
-				{
-					provide: getModelToken('Team'),
-					useValue: {}
-				},
-				{
-					provide: getModelToken('TeamUser'),
-					useValue: {}
-				},
-				{
-					provide: getModelToken('ResetPassword'),
-					useValue: {}
+					provide: TYPES.services.GetUserService,
+					useValue: createMock<GetUserServiceInterface>()
 				}
 			]
 		}).compile();
 		authenticationService = await module.get(ValidateUserAuthService);
-		gUserService = await module.get(TYPES.services.GetUserService);
+		getUserServiceMock = await module.get(TYPES.services.GetUserService);
 	});
+
+	beforeEach(() => {
+		jest.restoreAllMocks();
+		jest.clearAllMocks();
+
+		getUserServiceMock.getByEmail.mockResolvedValue(user);
+		bcryptCompare = jest.fn().mockReturnValue(true);
+		(bcrypt.compare as jest.Mock) = bcryptCompare;
+	});
+
 	describe('when accessing the data of authenticating user', () => {
 		it('should attempt to get a user by email', async () => {
-			const getByEmailSpy = jest.spyOn(gUserService, 'getByEmail');
 			await authenticationService.validateUserWithCredentials('user@email.com', 'strongPassword');
-			expect(getByEmailSpy).toBeCalledTimes(1);
+			expect(getUserServiceMock.getByEmail).toBeCalledTimes(1);
 		});
 		describe('and the provided password is not valid', () => {
 			beforeEach(() => {
@@ -114,10 +59,7 @@ describe('The AuthenticationService', () => {
 			});
 			it('should throw an error', async () => {
 				expect(
-					await authenticationService.validateUserWithCredentials(
-						'user@email.com',
-						'strongPassword'
-					)
+					await authenticationService.validateUserWithCredentials(user.email, 'strongPassword')
 				).toEqual(null);
 			});
 		});
@@ -127,22 +69,18 @@ describe('The AuthenticationService', () => {
 			});
 			describe('and the user is found in the database', () => {
 				it('should return the user data', async () => {
-					const user = await authenticationService.validateUserWithCredentials(
-						'user@email.com',
-						'strongPassword'
+					const userResult = await authenticationService.validateUserWithCredentials(
+						user.email,
+						user.password
 					);
-					expect(user).toBe(mockedUser);
+					expect(userResult).toBe(user);
 				});
 			});
 			describe('and the user is not found in the database', () => {
-				beforeEach(() => {
-					findUser.mockImplementation(() => ({
-						exec: jest.fn().mockReturnValue(undefined)
-					}));
-				});
 				it('should throw an error', async () => {
+					getUserServiceMock.getByEmail.mockResolvedValue(null);
 					await expect(
-						authenticationService.validateUserWithCredentials('user@email.com', 'strongPassword')
+						authenticationService.validateUserWithCredentials(user.email, 'strongPassword')
 					).resolves.toEqual(null);
 				});
 			});
